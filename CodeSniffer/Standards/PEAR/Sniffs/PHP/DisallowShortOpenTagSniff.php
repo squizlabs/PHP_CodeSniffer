cc47775b89846c2b7c4a3a0095b5f86f542a0468
Greg Sherwood <gsherwood@squiz.net>
2006-09-28 01:27:46 +0000

    Moved short ope tag sniff from Misc to PHP and renamed it. It also extends the generic sniff now.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@220650 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/PEAR/Sniffs/PHP/DisallowShortOpenTagSniff.php
+++ CodeSniffer/Standards/PEAR/Sniffs/PHP/DisallowShortOpenTagSniff.php
@@ -30,10 +30,10 @@
  * @author   Squiz Pty Ltd
  */
 
-require_once 'PHP/CodeSniffer/Sniff.php';
+require_once 'PHP/CodeSniffer/Standards/Generic/Sniffs/PHP/DisallowShortOpenTagSniff.php';
 
 /**
- * PEAR_Sniffs_Misc_ShortOpenTagSniff.
+ * PEAR_Sniffs_PHP_DisallowShortOpenTagSniff.
  *
  * Makes sure that shorthand PHP open tags are not used.
  *
@@ -41,63 +41,8 @@ require_once 'PHP/CodeSniffer/Sniff.php';
  * @category PEAR_Coding_Standards
  * @author   Squiz Pty Ltd
  */
-class PEAR_Sniffs_Misc_ShortOpenTagSniff implements PHP_CodeSniffer_Sniff
+class PEAR_Sniffs_PHP_DisallowShortOpenTagSniff extends Generic_Sniffs_PHP_DisallowShortOpenTagSniff
 {
-
-
-    /**
-     * Returns an array of tokens this test wants to listen for.
-     *
-     * @return array
-     */
-    public function register()
-    {
-        return array(
-                T_OPEN_TAG,
-                T_OPEN_TAG_WITH_ECHO,
-               );
-
-    }//end register()
-
-
-    /**
-     * Processes this test, when one of its tokens is encountered.
-     *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the
-     *                                        stack passed in $tokens.
-     *
-     * @return void
-     */
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-    {
-        // If short open tags are off, then any short open tags will be converted
-        // to inline_html tags so we can just ignore them.
-        // If its on, then we want to ban the use of them.
-        $option = ini_get('short_open_tag');
-
-        // Ini_get returns a string "0" if short open tags is off.
-        if ($option === '0') {
-            return;
-        }
-
-        $tokens = $phpcsFile->getTokens();
-        $openTag = $tokens[$stackPtr];
-
-        if ($openTag['content'] === '<?') {
-            $error = 'Short PHP opening tag used. Found "'.$openTag['content'].'" Expected "<?php".';
-            $phpcsFile->addError($error, $stackPtr);
-        }
-
-        if ($openTag['code'] === T_OPEN_TAG_WITH_ECHO) {
-            $nextVar  = $tokens[$phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, $stackPtr + 1, null, true)];
-            $error    = 'Short PHP opening tag used with echo. Found "';
-            $error   .= $openTag['content'].' '.$nextVar['content'].' ..." but expected "<?php echo '.$nextVar['content'].' ...".';
-            $phpcsFile->addError($error, $stackPtr);
-        }
-
-    }//end process()
-
 
 }//end class
 
