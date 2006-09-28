e5b2122339ec36cea796366090a0c5226fbccb7e
Greg Sherwood <gsherwood@squiz.net>
2006-09-28 01:24:11 +0000

    New generic sniffs that can be extended and used within your own coding standard to cut down on code and test duplication.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@220649 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Generic/Sniffs/PHP/LowerCaseConstantSniff.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * +------------------------------------------------------------------------+
+ * | BSD Licence                                                            |
+ * +------------------------------------------------------------------------+
+ * | This software is available to you under the BSD license,               |
+ * | available in the LICENSE file accompanying this software.              |
+ * | You may obtain a copy of the License at                                |
+ * |                                                                        |
+ * | http://matrix.squiz.net/developer/tools/php_cs/licence                 |
+ * |                                                                        |
+ * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    |
+ * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      |
+ * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  |
+ * | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   |
+ * | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  |
+ * | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       |
+ * | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  |
+ * | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  |
+ * | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    |
+ * | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  |
+ * | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   |
+ * +------------------------------------------------------------------------+
+ * | Copyright (c), 2006 Squiz Pty Ltd (ABN 77 084 670 600).                |
+ * | All rights reserved.                                                   |
+ * +------------------------------------------------------------------------+
+ *
+ * @package  PHP_CodeSniffer
+ * @category Squiz_Coding_Standards
+ * @author   Squiz Pty Ltd
+ */
+
+require_once 'PHP/CodeSniffer/Sniff.php';
+
+
+/**
+ * Generic_Sniffs_PHP_LowerCaseConstantSniff.
+ *
+ * Checks that all uses of true, false and null are lowerrcase.
+ *
+ * @package  PHP_CodeSniffer
+ * @category Generic_Coding_Standards
+ * @author   Squiz Pty Ltd
+ */
+class Generic_Sniffs_PHP_LowerCaseConstantSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_TRUE,
+                T_FALSE,
+                T_NULL,
+               );
+
+    }//end register()
+
+
+    /**
+     * Processes this sniff, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile  The file being scanned.
+     * @param int                  $stackPtr   The position of the current token in the
+     *                                         stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $keyword = $tokens[$stackPtr]['content'];
+        if (strtolower($keyword) !== $keyword) {
+            $error = 'PHP Keywords should be lowercase case. Expected "'.strtolower($keyword).'" but found "'.$keyword.'".';
+            $phpcsFile->addError($error, $stackPtr);
+        }
+
+    }//end process()
+
+
+}//end class
+
+?>
