7811d6df0637a5061d7b531a73407d2381c623b1
Greg Sherwood <gsherwood@squiz.net>
2006-09-28 07:07:27 +0000

    Sniff that checks valid constant names moved to NamingConventions category and now using generic sniff
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@220664 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/PEAR/Sniffs/NamingConventions/ValidConstantNameSniff.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * +------------------------------------------------------------------------+
+ * | BSD Licence                                                            |
+ * +------------------------------------------------------------------------+
+ * | This software is available to you under the BSD license,               |
+ * | available in the LICENSE file accompanying this software.              |
+ * | You may obtain a copy of the License at                                |
+ * |                                                                        |
+ * | http://matrix.squiz.net/developer/tools/php_cs/licence                 |
+ * |                                                                        |
+ * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    |
+ * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      |
+ * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  |
+ * | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   |
+ * | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  |
+ * | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       |
+ * | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  |
+ * | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  |
+ * | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    |
+ * | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  |
+ * | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   |
+ * +------------------------------------------------------------------------+
+ * | Copyright (c), 2006 Squiz Pty Ltd (ABN 77 084 670 600).                |
+ * | All rights reserved.                                                   |
+ * +------------------------------------------------------------------------+
+ *
+ * @package  PHP_CodeSniffer
+ * @category PEAR_Coding_Standards
+ * @author   Squiz Pty Ltd
+ */
+
+require_once 'PHP/CodeSniffer/Standards/Generic/Sniffs/NamingConventions/UpperCaseConstantNameSniff.php';
+
+
+/**
+ * PEAR_Sniffs_NamingConventions_ValidConstantNameSniff.
+ *
+ * Ensures that constant names are all uppercase.
+ *
+ * @package  PHP_CodeSniffer
+ * @category PEAR_Coding_Standards
+ * @author   Squiz Pty Ltd
+ */
+class PEAR_Sniffs_NamingConventions_ValidConstantNameSniff extends Generic_Sniffs_NamingConventions_UpperCaseConstantNameSniff
+{
+
+
+}//end class
+
+?>
