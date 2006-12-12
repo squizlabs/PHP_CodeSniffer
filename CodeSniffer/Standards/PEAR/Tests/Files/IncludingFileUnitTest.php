f692783cde3ab523e3a25c817fa8d0fe83e6f0dd
Greg Sherwood <gsherwood@squiz.net>
2006-12-12 02:57:38 +0000

    Updated doc comments
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@224870 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/PEAR/Tests/Files/IncludingFileUnitTest.php
+++ CodeSniffer/Standards/PEAR/Tests/Files/IncludingFileUnitTest.php
@@ -1,32 +1,17 @@
 <?php
 /**
- * +------------------------------------------------------------------------+
- * | BSD Licence                                                            |
- * +------------------------------------------------------------------------+
- * | This software is available to you under the BSD license,               |
- * | available in the LICENSE file accompanying this software.              |
- * | You may obtain a copy of the License at                                |
- * |                                                                        |
- * | http://matrix.squiz.net/developer/tools/php_cs/licence                 |
- * |                                                                        |
- * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    |
- * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      |
- * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  |
- * | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT   |
- * | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  |
- * | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       |
- * | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,  |
- * | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY  |
- * | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT    |
- * | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  |
- * | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   |
- * +------------------------------------------------------------------------+
- * | Copyright (c), 2006 Squiz Pty Ltd (ABN 77 084 670 600).                |
- * | All rights reserved.                                                   |
- * +------------------------------------------------------------------------+
+ * Unit test class for the IncludingFile sniff.
  *
- * @package PHP_CodeSniffer
- * @author  Squiz Pty Ltd
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 
 /**
@@ -35,8 +20,14 @@
  * A sniff unit test checks a .inc file for expected violations of a single
  * coding standard. Expected errors and warnings are stored in this class.
  *
- * @package PHP_CodeSniffer
- * @author  Squiz Pty Ltd
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 class PEAR_Tests_Files_IncludingFileUnitTest extends AbstractSniffUnitTest
 {
