7f202b59ee6316650b3e17549f1cbe5a37869f38
Greg Sherwood <gsherwood@squiz.net>
2006-12-21 02:31:14 +0000

    Added MultipleStatementAlignment to generic standard - checks that mutliple statements have their equals sign aligned.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@225452 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Generic/Tests/Formatting/MultipleStatementAlignmentUnitTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Unit test class for the MultipleStatementAlignment sniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Unit test class for the MultipleStatementAlignment sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Generic_Tests_Statements_MultipleStatementAlignmentUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList()
+    {
+        return array();
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array(
+                11 => 1,
+                12 => 1,
+                23 => 1,
+                24 => 1,
+                26 => 1,
+                27 => 1,
+                37 => 1,
+                38 => 1,
+                48 => 1,
+                50 => 1,
+                61 => 1,
+                62 => 1,
+                64 => 1,
+                65 => 1,
+               );
+
+    }//end getWarningList()
+
+
+}//end class
+
+?>
