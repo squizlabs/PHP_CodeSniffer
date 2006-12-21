db24a82050944dce907c4d35b9c63ebec31ecda9
Greg Sherwood <gsherwood@squiz.net>
2006-12-21 02:33:09 +0000

    Moved MultipleStatementSniff into Formatting category, renamed it, and used Generic sniff to implement logic.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@225453 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/PEAR/Sniffs/Formatting/MultipleStatementAlignmentSniff.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * PEAR_Sniffs_Statements_MultipleStatementSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+require_once 'PHP/CodeSniffer/Standards/Generic/Sniffs/Formatting/MultipleStatementAlignmentSniff.php';
+
+/**
+ * PEAR_Sniffs_Statements_MultipleStatementSniff.
+ *
+ * Checks alignment of assignments. If there are multiple adjacent assignments,
+ * it will check that the equals signs of each assignment are aligned. It will
+ * display a warning to advise that the signs should be aligned.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PEAR_Sniffs_Statements_MultipleStatementSniff extends Generic_Sniffs_Statements_MultipleStatementSniff
+{
+
+}//end class
+
+?>
