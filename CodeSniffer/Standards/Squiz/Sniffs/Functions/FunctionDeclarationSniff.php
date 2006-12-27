89efe6ea718192de1b0b88fa005ae51a5421fce6
Greg Sherwood <gsherwood@squiz.net>
2006-12-27 23:18:37 +0000

    Sniff added to Squiz coding standard
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@225905 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Squiz/Sniffs/Functions/FunctionDeclarationSniff.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Squiz_Sniffs_Functions_FunctionDeclarationSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+require_once 'PHP/CodeSniffer/Standards/AbstractPatternSniff.php';
+
+/**
+ * Squiz_Sniffs_Functions_FunctionDeclarationSniff.
+ *
+ * Checks the function declaration is correct.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Squiz_Sniffs_Functions_FunctionDeclarationSniff extends PHP_CodeSniffer_Standards_AbstractPatternSniff
+{
+
+
+    /**
+     * Returns an array of patterns to check are correct.
+     *
+     * @return array
+     */
+    protected function getPatterns()
+    {
+        return array(
+                'function abc(...);',
+                'abstract function abc(...);',
+                "function abc(...)\n",
+               );
+
+    }//end getPatterns()
+
+
+}//end class
+
+?>
