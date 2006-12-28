b0777fb3a66aa1dc35445a7d61454a79817950dd
Greg Sherwood <gsherwood@squiz.net>
2006-12-28 06:01:17 +0000

    New generic sniff that allows you to forbid the use of any functions you want. Defaults to forbid the use of alias functions.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@225921 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Generic/Sniffs/PHP/ForbiddenFunctionsSniff.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Generic_Sniffs_PHP_ForbiddenFunctionsSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+require_once 'PHP/CodeSniffer/Sniff.php';
+
+/**
+ * Generic_Sniffs_PHP_ForbiddenFunctionsSniff.
+ *
+ * Discourages the use of alias functions that are kept in PHP for compatibility
+ * with older versions. Can be used to forbid the use of any function.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Generic_Sniffs_PHP_ForbiddenFunctionsSniff implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * A list of forbidden functions with their alternatives.
+     *
+     * The value is NULL if no alternative exists. IE, the
+     * function should just not be used.
+     *
+     * @var array(string => string|null)
+     */
+    protected $forbiddenFunctions = array(
+                                     'sizeof' => 'count',
+                                     'delete' => 'unset',
+                                    );
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_STRING,
+               );
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $function = strtolower($tokens[$stackPtr]['content']);
+
+        if (!in_array($function, array_keys($this->forbiddenFunctions))) {
+            return;
+        }
+
+        $error = "The use of function $function() is forbidden";
+        if ($this->forbiddenFunctions[$function] !== null) {
+            $error .= '; use '.$this->forbiddenFunctions[$function].'() instead';
+        }
+
+        $phpcsFile->addError($error, $stackPtr);
+
+    }//end process()
+
+
+}//end class
+
+?>
