ecae5c7ad29936011aeed509ea54d42cb218fb8e
Greg Sherwood <gsherwood@squiz.net>
2007-03-01 01:01:10 +0000

    Errors no longer thrown for class method names in Generic ForbiddenFunctionsSniff
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@230968 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/Generic/Sniffs/PHP/ForbiddenFunctionsSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/PHP/ForbiddenFunctionsSniff.php
@@ -75,6 +75,12 @@ class Generic_Sniffs_PHP_ForbiddenFunctionsSniff implements PHP_CodeSniffer_Snif
     {
         $tokens = $phpcsFile->getTokens();
 
+        $prevToken = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
+        if (in_array($tokens[$prevToken]['code'], array(T_DOUBLE_COLON, T_OBJECT_OPERATOR)) === true) {
+            // Not a call to a PHP function.
+            return;
+        }
+
         $function = strtolower($tokens[$stackPtr]['content']);
 
         if (in_array($function, array_keys($this->forbiddenFunctions)) === false) {
