6a7049c0dba5d1cc807baa30c2e651af42f0357a
Greg Sherwood <gsherwood@squiz.net>
2007-04-04 04:28:57 +0000

    Errors no longer thrown for user-defined functions named as forbidden functions in Generic ForbiddenFunctionsSniff
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@233243 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/Generic/Sniffs/PHP/ForbiddenFunctionsSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/PHP/ForbiddenFunctionsSniff.php
@@ -76,7 +76,7 @@ class Generic_Sniffs_PHP_ForbiddenFunctionsSniff implements PHP_CodeSniffer_Snif
         $tokens = $phpcsFile->getTokens();
 
         $prevToken = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
-        if (in_array($tokens[$prevToken]['code'], array(T_DOUBLE_COLON, T_OBJECT_OPERATOR)) === true) {
+        if (in_array($tokens[$prevToken]['code'], array(T_DOUBLE_COLON, T_OBJECT_OPERATOR, T_FUNCTION)) === true) {
             // Not a call to a PHP function.
             return;
         }
