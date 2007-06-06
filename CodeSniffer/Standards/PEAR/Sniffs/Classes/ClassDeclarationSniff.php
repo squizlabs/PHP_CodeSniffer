c46053540c83d554742a98345e42107818d8b4e0
Greg Sherwood <gsherwood@squiz.net>
2007-06-06 23:32:20 +0000

    New sniff to enforce new PEAR standard that the opening brace of a class must be on the line following the class keyword
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@237137 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/PEAR/Sniffs/Classes/ClassDeclarationSniff.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Class Declaration Test.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+require_once 'PHP/CodeSniffer/Sniff.php';
+
+/**
+ * Class Declaration Test.
+ *
+ * Checks the declaration of the class is correct.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PEAR_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_CLASS,
+                T_INTERFACE,
+               );
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $curlyBrace = $tokens[$stackPtr]['scope_opener'];
+        $classLine  = $tokens[$stackPtr]['line'];
+        $braceLine  = $tokens[$curlyBrace]['line'];
+        if ($braceLine === $classLine) {
+            $error  = 'Opening brace of a ';
+            $error .= $tokens[$stackPtr]['content'];
+            $error .= ' must be on the line after the definition.';
+            $phpcsFile->addError($error, $stackPtr);
+            return;
+        } else if ($braceLine > ($classLine + 1)) {
+            $difference  = ($braceLine - $classLine - 1);
+            $difference .= ($difference === 1) ? ' empty line' : ' empty lines';
+            $error       = 'Opening brace of a ';
+            $error      .= $tokens[$stackPtr]['content'];
+            $error      .= ' must be on the line following the ';
+            $error      .= $tokens[$stackPtr]['content'];
+            $error      .= ' declaration; found '.$difference;
+            $phpcsFile->addError($error, $curlyBrace);
+            return;
+        }
+
+        if ($tokens[($curlyBrace + 1)]['content'] !== $phpcsFile->eolChar) {
+            $type  = strtolower($tokens[$stackPtr]['content']);
+            $error = "Opening $type brace must be on a line by itself";
+            $phpcsFile->addError($error, $curlyBrace);
+        }
+
+        if ($tokens[($curlyBrace - 1)]['code'] === T_WHITESPACE) {
+            $prevContent = $tokens[($curlyBrace - 1)]['content'];
+            if ($prevContent !== $phpcsFile->eolChar) {
+                $blankSpace = substr($prevContent, strpos($prevContent, $phpcsFile->eolChar));
+                $spaces     = strlen($blankSpace);
+                if ($spaces !== 0) {
+                    $error = "Expected 0 spaces before opening brace; $spaces found";
+                    $phpcsFile->addError($error, $curlyBrace);
+                }
+            }
+        }
+
+    }//end process()
+
+
+}//end class
+
+?>
