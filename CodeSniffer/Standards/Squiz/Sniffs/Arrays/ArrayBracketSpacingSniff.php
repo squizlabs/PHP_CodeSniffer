bccc6cf4e9f989ddef4584bb0730d59658fb66ac
Greg Sherwood <gsherwood@squiz.net>
2007-10-09 03:38:35 +0000

    Added new Squiz ArrayBracketSpacingSniff to throw errors if whitespace is found around square brackets
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@243868 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Squiz/Sniffs/Arrays/ArrayBracketSpacingSniff.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Squiz_Sniffs_Arrays_ArrayBracketSpacingSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Squiz_Sniffs_Arrays_ArrayBracketSpacingSniff.
+ *
+ * Ensure that there are no spaces around square brackets.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Squiz_Sniffs_Arrays_ArrayBracketSpacingSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_OPEN_SQUARE_BRACKET,
+                T_CLOSE_SQUARE_BRACKET,
+               );
+
+    }//end register()
+
+
+    /**
+     * Processes this sniff, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The current file being checked.
+     * @param int                  $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        // No bracket can have space before them.
+        $prevType = $tokens[($stackPtr - 1)]['code'];
+        if (in_array($prevType, PHP_CodeSniffer_Tokens::$emptyTokens) === true) {
+            $nonSpace = $phpcsFile->findPrevious(PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr - 2), null, true);
+            $expected = $tokens[$nonSpace]['content'].$tokens[$stackPtr]['content'];
+            $found    = $phpcsFile->getTokensAsString($nonSpace, ($stackPtr - $nonSpace)).$tokens[$stackPtr]['content'];
+            $error    = "Space found before square bracket; expected \"$expected\" but found \"$found\"";
+            $phpcsFile->addError($error, $stackPtr);
+        }
+
+        if ($tokens[$stackPtr]['type'] === 'T_OPEN_SQUARE_BRACKET') {
+            // Open brackets can't have spaces on after them either.
+            $nextType = $tokens[($stackPtr + 1)]['code'];
+            if (in_array($nextType, PHP_CodeSniffer_Tokens::$emptyTokens) === true) {
+                $nonSpace = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr + 2), null, true);
+                $expected = $tokens[$stackPtr]['content'].$tokens[$nonSpace]['content'];
+                $found    = $phpcsFile->getTokensAsString($stackPtr, ($nonSpace- $stackPtr + 1));
+                $error    = "Space found after square bracket; expected \"$expected\" but found \"$found\"";
+                $phpcsFile->addError($error, $stackPtr);
+            }
+        }
+
+    }//end process()
+
+
+}//end class
+
+?>
