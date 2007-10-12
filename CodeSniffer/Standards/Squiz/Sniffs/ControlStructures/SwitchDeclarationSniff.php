c257a6964570c8b8cc652e15d16d80bca70bf25c
Greg Sherwood <gsherwood@squiz.net>
2007-10-12 00:02:48 +0000

    Lots of new tests inside the switch sniff to make it more strict.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@244087 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/Squiz/Sniffs/ControlStructures/SwitchDeclarationSniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/ControlStructures/SwitchDeclarationSniff.php
@@ -18,7 +18,7 @@
  * Squiz_Sniffs_ControlStructures_SwitchDeclarationSniff.
  *
  * Ensures all the breaks and cases are aligned correctly according to their
- * parent switch's alignment.
+ * parent switch's alignment and enforces other switch formatting.
  *
  * @category  PHP
  * @package   PHP_CodeSniffer
@@ -75,6 +75,17 @@ class Squiz_Sniffs_ControlStructures_SwitchDeclarationSniff implements PHP_CodeS
                 $phpcsFile->addError($error, $nextCase);
             }
 
+            if ($tokens[($nextCase + 1)]['type'] !== 'T_WHITESPACE' || $tokens[($nextCase + 1)]['content'] !== ' ') {
+                $error = 'CASE keyword must be followed by a single space';
+                $phpcsFile->addError($error, $nextCase);
+            }
+
+            $opener = $tokens[$nextCase]['scope_opener'];
+            if ($tokens[($opener - 1)]['type'] === 'T_WHITESPACE') {
+                $error = 'There must be no space before the colon in a CASE statement';
+                $phpcsFile->addError($error, $nextCase);
+            }
+
             $nextBreak = $phpcsFile->findNext(array(T_BREAK), ($nextCase + 1), $switch['scope_closer']);
             if ($nextBreak !== false && isset($tokens[$nextBreak]['scope_condition']) === true) {
                 // Only check this BREAK statement if it matches the current CASE
@@ -85,9 +96,67 @@ class Squiz_Sniffs_ControlStructures_SwitchDeclarationSniff implements PHP_CodeS
                         $error = 'BREAK statement must be indented 4 spaces from SWITCH keyword';
                         $phpcsFile->addError($error, $nextBreak);
                     }
-                }
+
+                    /*
+                        Ensure there is no blank line before
+                        the BREAK statement.
+                    */
+
+                    $breakLine = $tokens[$nextBreak]['line'];
+                    $prevLine  = 0;
+                    for ($i = ($nextBreak - 1); $i > $stackPtr; $i--) {
+                        if ($tokens[$i]['type'] !== 'T_WHITESPACE') {
+                            $prevLine = $tokens[$i]['line'];
+                            break;
+                        }
+                    }
+
+                    if ($prevLine !== ($breakLine - 1)) {
+                        $error = 'Blank lines are not allowed before BREAK statements';
+                        $phpcsFile->addError($error, $nextBreak);
+                    }
+
+                    /*
+                        Ensure the BREAK statement is followed by
+                        a single blank line, or the end switch brace.
+                    */
+
+                    $breakLine = $tokens[$nextBreak]['line'];
+                    $nextLine  = $tokens[$tokens[$stackPtr]['scope_closer']]['line'];
+                    $semicolon = $phpcsFile->findNext(T_SEMICOLON, $nextBreak);
+                    for ($i = ($semicolon + 1); $i < $tokens[$stackPtr]['scope_closer']; $i++) {
+                        if ($tokens[$i]['type'] !== 'T_WHITESPACE') {
+                            $nextLine = $tokens[$i]['line'];
+                            break;
+                        }
+                    }
+
+                    if ($nextLine !== ($breakLine + 2) && $i !== $tokens[$stackPtr]['scope_closer']) {
+                        $error = 'BREAK statements must be followed by a single blank line';
+                        $phpcsFile->addError($error, $nextBreak);
+                    }
+                }//end if
             } else {
                 $nextBreak = $tokens[$nextCase]['scope_closer'];
+            }//end if
+
+            /*
+                Ensure CASE statements are not followed by
+                blank lines.
+            */
+
+            $caseLine = $tokens[$nextCase]['line'];
+            $nextLine = $tokens[$nextBreak]['line'];
+            for ($i = ($opener + 1); $i < $nextBreak; $i++) {
+                if ($tokens[$i]['type'] !== 'T_WHITESPACE') {
+                    $nextLine = $tokens[$i]['line'];
+                    break;
+                }
+            }
+
+            if ($nextLine !== ($caseLine + 1)) {
+                $error = 'Blank lines are not allowed after CASE statements';
+                $phpcsFile->addError($error, $nextCase);
             }
         }//end while
 
@@ -97,6 +166,12 @@ class Squiz_Sniffs_ControlStructures_SwitchDeclarationSniff implements PHP_CodeS
             if ($content !== strtolower($content)) {
                 $expected = strtolower($content);
                 $error    = "DEFAULT keyword must be lowercase; expected \"$expected\" but found \"$content\"";
+                $phpcsFile->addError($error, $default);
+            }
+
+            $opener = $tokens[$default]['scope_opener'];
+            if ($tokens[($opener - 1)]['type'] === 'T_WHITESPACE') {
+                $error = 'There must be no space before the colon in a DEFAULT statement';
                 $phpcsFile->addError($error, $default);
             }
 
@@ -111,9 +186,74 @@ class Squiz_Sniffs_ControlStructures_SwitchDeclarationSniff implements PHP_CodeS
                     $error = 'BREAK statement must be indented 4 spaces from SWITCH keyword';
                     $phpcsFile->addError($error, $nextBreak);
                 }
+
+                /*
+                    Ensure the BREAK statement is not followed by
+                    a blank line.
+                */
+
+                $breakLine = $tokens[$nextBreak]['line'];
+                $nextLine  = $tokens[$tokens[$stackPtr]['scope_closer']]['line'];
+                $semicolon = $phpcsFile->findNext(T_SEMICOLON, $nextBreak);
+                for ($i = ($semicolon + 1); $i < $tokens[$stackPtr]['scope_closer']; $i++) {
+                    if ($tokens[$i]['type'] !== 'T_WHITESPACE') {
+                        $nextLine = $tokens[$i]['line'];
+                        break;
+                    }
+                }
+
+                if ($nextLine !== ($breakLine + 1)) {
+                    $error = 'Blank lines are not allowed after the DEFAULT case\'s BREAK statement';
+                    $phpcsFile->addError($error, $nextBreak);
+                }
             } else {
+                $error = 'DEFAULT case must have a BREAK statement';
+                $phpcsFile->addError($error, $default);
+
                 $nextBreak = $tokens[$default]['scope_closer'];
+            }//end if
+
+            /*
+                Ensure empty DEFAULT statements are not allowed.
+                They must (at least) have a comment describing why
+                the default case is being ignored.
+            */
+
+            $foundContent = false;
+            for ($i = ($tokens[$default]['scope_opener'] + 1); $i < $nextBreak; $i++) {
+                if ($tokens[$i]['type'] !== 'T_WHITESPACE') {
+                    $foundContent = true;
+                    break;
+                }
             }
+
+            if ($foundContent === false) {
+                $error = 'Comment required for empty DEFAULT case';
+                $phpcsFile->addError($error, $default);
+            }
+
+            /*
+                Ensure DEFAULT statements are not followed by
+                blank lines.
+            */
+
+            $defaultLine = $tokens[$default]['line'];
+            $nextLine    = $tokens[$nextBreak]['line'];
+            for ($i = ($opener + 1); $i < $nextBreak; $i++) {
+                if ($tokens[$i]['type'] !== 'T_WHITESPACE') {
+                    $nextLine = $tokens[$i]['line'];
+                    break;
+                }
+            }
+
+            if ($nextLine !== ($defaultLine + 1)) {
+                $error = 'Blank lines are not allowed after DEFAULT statements';
+                $phpcsFile->addError($error, $default);
+            }
+
+        } else {
+            $error = 'All SWITCH statements must contain a DEFAULT case';
+            $phpcsFile->addError($error, $stackPtr);
         }//end if
 
         if ($tokens[$switch['scope_closer']]['column'] !== $switch['column']) {
