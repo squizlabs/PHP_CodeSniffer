2d7ff2ab1dbc48f411a8c2b76217384ff5395f59
Greg Sherwood <gsherwood@squiz.net>
2007-11-04 22:02:16 +0000

    Fixed bug #12368 : Autoloader cannot be found due to include_path override. phpcs.bat not overrides auto_append and auto_prepend in case that reference files outside of PEAR.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@245545 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/phpcs.bat
+++ scripts/phpcs.bat
@@ -10,7 +10,7 @@ REM @author    Greg Sherwood <gsherwood@squiz.net>
 REM @author    Marc McIntyre <mmcintyre@squiz.net>
 REM @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
 REM @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
-REM @version   CVS: $Id: phpcs.bat,v 1.2 2006-12-11 23:45:27 squiz Exp $
+REM @version   CVS: $Id: phpcs.bat,v 1.3 2007-11-04 22:02:16 squiz Exp $
 REM @link      http://pear.php.net/package/PHP_CodeSniffer
 
-"@php_bin@" -d include_path="@php_dir@" "@bin_dir@\phpcs" %*
+"@php_bin@" -d auto_append_file="" -d auto_prepend_file="" -d include_path="@php_dir@" "@bin_dir@\phpcs" %*
