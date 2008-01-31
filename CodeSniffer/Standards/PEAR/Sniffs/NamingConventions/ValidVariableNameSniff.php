5311944e45481af412bb16d7b9c744bbcb8e6288
Greg Sherwood <gsherwood@squiz.net>
2008-01-31 22:17:02 +0000

    Added PEAR ValidVariableNameSniff to check that only private member vars are prefixed with an underscore
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@251886 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/PEAR/Sniffs/NamingConventions/ValidVariableNameSniff.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * PEAR_Sniffs_NamingConventions_ValidVariableNameSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+if (class_exists('PHP_CodeSniffer_Standards_AbstractVariableSniff', true) === false) {
+    $error = 'Class PHP_CodeSniffer_Standards_AbstractVariableSniff not found';
+    throw new PHP_CodeSniffer_Exception($error);
+}
+
+/**
+ * PEAR_Sniffs_NamingConventions_ValidVariableNameSniff.
+ *
+ * Checks the naming of member variables.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PEAR_Sniffs_NamingConventions_ValidVariableNameSniff extends PHP_CodeSniffer_Standards_AbstractVariableSniff
+{
+
+
+    /**
+     * Processes class member variables.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    protected function processMemberVar(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $memberName     = ltrim($tokens[$stackPtr]['content'], '$');
+        $memberProps    = $phpcsFile->getMemberProperties($stackPtr);
+        $isPublic       = ($memberProps['scope'] === 'private') ? false : true;
+        $scope          = $memberProps['scope'];
+        $scopeSpecified = $memberProps['scope_specified'];
+
+        // If it's a private member, it must have an underscore on the front.
+        if ($isPublic === false && $memberName{0} !== '_') {
+            $error = "Private member variable \"$memberName\" must be prefixed with an underscore";
+            $phpcsFile->addError($error, $stackPtr);
+            return;
+        }
+
+        // If it's not a private member, it must not have an underscore on the front.
+        if ($isPublic === true && $scopeSpecified === true && $memberName{0} === '_') {
+            $error = ucfirst($scope)." member variable \"$memberName\" must not be prefixed with an underscore";
+            $phpcsFile->addError($error, $stackPtr);
+            return;
+        }
+
+    }//end processMemberVar()
+
+
+    /**
+     * Processes normal variables.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where this token was found.
+     * @param int                  $stackPtr  The position where the token was found.
+     *
+     * @return void
+     */
+    protected function processVariable(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        // We don't care about normal variables.
+        return;
+
+    }//end processVariable()
+
+
+    /**
+     * Processes variables in double quoted strings.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where this token was found.
+     * @param int                  $stackPtr  The position where the token was found.
+     *
+     * @return void
+     */
+    protected function processVariableInString(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        // We don't care about normal variables.
+        return;
+
+    }//end processVariableInString()
+
+
+}//end class
+
+?>
