80cb8f4c13d0cd973e9f9bccaa03c937f0dd77ed
Greg Sherwood <gsherwood@squiz.net>
2008-05-19 03:34:56 +0000

    Squiz standard now ensures array indexes are not camel case
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@259959 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/Squiz/Tests/NamingConventions/ValidArrayIndexNameUnitTest.php
+++ CodeSniffer/Standards/Squiz/Tests/NamingConventions/ValidArrayIndexNameUnitTest.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * Unit test class for the ArrayDeclaration sniff.
+ * Unit test class for the ValidArrayIndexName sniff.
  *
  * PHP version 5
  *
  * @category  PHP
  * @package   PHP_CodeSniffer
  * @author    Greg Sherwood <gsherwood@squiz.net>
- * @author    Marc McIntyre <mmcintyre@squiz.net>
  * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
  * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
  * @version   CVS: $Id$
@@ -15,7 +14,7 @@
  */
 
 /**
- * Unit test class for the ArrayDeclaration sniff.
+ * Unit test class for the ValidArrayIndexName sniff.
  *
  * A sniff unit test checks a .inc file for expected violations of a single
  * coding standard. Expected errors and warnings are stored in this class.
@@ -23,13 +22,12 @@
  * @category  PHP
  * @package   PHP_CodeSniffer
  * @author    Greg Sherwood <gsherwood@squiz.net>
- * @author    Marc McIntyre <mmcintyre@squiz.net>
  * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
  * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
  * @version   Release: @package_version@
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
-class Squiz_Tests_Arrays_ArrayDeclarationUnitTest extends AbstractSniffUnitTest
+class Squiz_Tests_NamingConventions_ValidArrayIndexNameUnitTest extends AbstractSniffUnitTest
 {
 
 
@@ -44,55 +42,8 @@ class Squiz_Tests_Arrays_ArrayDeclarationUnitTest extends AbstractSniffUnitTest
     public function getErrorList()
     {
         return array(
-                7   => 2,
-                9   => 2,
-                22  => 1,
-                23  => 1,
-                24  => 1,
-                25  => 1,
-                31  => 1,
-                35  => 1,
-                36  => 1,
-                41  => 1,
-                46  => 1,
-                47  => 1,
-                50 =>  1,
-                51  => 1,
-                53  => 1,
-                56  => 1,
-                58  => 1,
-                61  => 1,
-                62  => 1,
-                63  => 1,
-                64  => 1,
-                65  => 1,
-                66  => 3,
-                70  => 1,
-                76  => 2,
-                77  => 1,
-                78  => 7,
-                79  => 2,
-                81  => 2,
-                82  => 4,
-                87  => 1,
-                88  => 1,
-                92  => 1,
-                97  => 1,
-                100 => 1,
-                101 => 1,
-                102 => 1,
-                105 => 1,
-                106 => 1,
-                107 => 1,
-                125 => 1,
-                126 => 1,
-                141 => 1,
-                144 => 1,
-                146 => 1,
-                148 => 1,
-                151 => 1,
-                157 => 1,
-                162 => 1,
+                3 => 1,
+                8 => 1,
                );
 
     }//end getErrorList()
