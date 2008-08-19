bf898b87c68e9d8e1480ff51120757de1c83f5d1
Greg Sherwood <gsherwood@squiz.net>
2008-08-19 05:15:14 +0000

    Added Generic TodoSniff that warns about comments that contain the word TODO. The Squiz standard is currently the only standard using this sniff.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@265107 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/Squiz/SquizCodingStandard.php
+++ CodeSniffer/Standards/Squiz/SquizCodingStandard.php
@@ -45,6 +45,7 @@ class PHP_CodeSniffer_Standards_Squiz_SquizCodingStandard extends PHP_CodeSniffe
     public function getIncludedSniffs()
     {
         return array(
+                'Generic/Sniffs/Commenting/TodoSniff.php',
                 'Generic/Sniffs/ControlStructures/InlineControlStructureSniff.php',
                 'Generic/Sniffs/Formatting/DisallowMultipleStatementsSniff.php',
                 'Generic/Sniffs/Formatting/SpaceAfterCastSniff.php',
