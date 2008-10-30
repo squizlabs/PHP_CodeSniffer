8f8583dc16e3e43e0dda6fa59b08f63b036692f1
Greg Sherwood <gsherwood@squiz.net>
2008-10-30 05:35:40 +0000

    Added Squiz ClassDefinitionOpeningBraceSpaceSniff to check for spacing around opening braces in class definitions
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@267997 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Squiz/Sniffs/CSS/ClassDefinitionOpeningBraceSpaceSniff.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Squiz_Sniffs_CSS_ColonSpacingSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Squiz_Sniffs_CSS_ClassDefinitionOpeningBraceSpaceSniff.
+ *
+ * Ensure there is a single space before the opening brace in a class definition.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Squiz_Sniffs_CSS_ClassDefinitionOpeningBraceSpaceSniff implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array('CSS');
+
+
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array(int)
+     */
+    public function register()
+    {
+        return array(T_OPEN_CURLY_BRACKET);
+
+    }//end register()
+
+
+    /**
+     * Processes the tokens that this sniff is interested in.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where the token was found.
+     * @param int                  $stackPtr  The position in the stack where
+     *                                        the token was found.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if ($tokens[($stackPtr - 1)]['code'] !== T_WHITESPACE) {
+            $error = 'Expected 1 space before opening brace of class definition; 0 found';
+            $phpcsFile->addError($error, $stackPtr);
+        } else {
+            $content = $tokens[($stackPtr - 1)]['content'];
+            if ($content !== ' ') {
+                $length = strlen($content);
+                if ($length === 1) {
+                    $length = 'tab';
+                }
+
+                $error = "Expected 1 space before opening brace of class definition; $length found";
+                $phpcsFile->addError($error, $stackPtr);
+            }
+        }//end if
+
+    }//end process()
+
+}//end class
+?>
