c7abafd2ae6eddbc7289734383dd05e06834511d
Greg Sherwood <gsherwood@squiz.net>
2008-10-31 00:24:24 +0000

    Added Squiz ClassDefinitionClosingBraceSpaceSniff to check for a single blank line after CSS class definitions
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@268058 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Squiz/Sniffs/CSS/ClassDefinitionClosingBraceSpaceSniff.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Squiz_Sniffs_CSS_ColonSpacingSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Squiz_Sniffs_CSS_ClassDefinitionClosingBraceSpaceSniff.
+ *
+ * Ensure there is a single blank line after the closing brace of a class definition.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Squiz_Sniffs_CSS_ClassDefinitionClosingBraceSpaceSniff implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array('CSS');
+
+
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array(int)
+     */
+    public function register()
+    {
+        return array(T_CLOSE_CURLY_BRACKET);
+
+    }//end register()
+
+
+    /**
+     * Processes the tokens that this sniff is interested in.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where the token was found.
+     * @param int                  $stackPtr  The position in the stack where
+     *                                        the token was found.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $next = $phpcsFile->findNext(T_WHITESPACE, ($stackPtr + 1), null, true);
+        if ($next === false || $tokens[$next]['code'] === T_CLOSE_TAG) {
+            return;
+        }
+
+        $found = (($tokens[$next]['line'] - $tokens[$stackPtr]['line']) - 1);
+        if ($found !== 1) {
+            $error = "Expected one blank line after closing brace of class definition; $found found";
+            $phpcsFile->addError($error, $stackPtr);
+        }
+
+    }//end process()
+
+}//end class
+?>
