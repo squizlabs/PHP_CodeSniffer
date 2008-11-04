627d427ee2db41a50225d003dd9de28ec2135a24
Greg Sherwood <gsherwood@squiz.net>
2008-11-04 03:05:00 +0000

    Added Squiz DisallowMultipleStyleDefinitionsSniff to check for multiple style definitions on a single line
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@268250 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Squiz/Sniffs/CSS/DisallowMultipleStyleDefinitionsSniff.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Squiz_Sniffs_CSS_DisallowMultipleStyleDefinitionsSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Squiz_Sniffs_CSS_DisallowMultipleStyleDefinitionsSniff.
+ *
+ * Ensure that style definitions are not empty.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Squiz_Sniffs_CSS_DisallowMultipleStyleDefinitionsSniff implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array('CSS');
+
+
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array(int)
+     */
+    public function register()
+    {
+        return array(T_STYLE);
+
+    }//end register()
+
+
+    /**
+     * Processes the tokens that this sniff is interested in.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file where the token was found.
+     * @param int                  $stackPtr  The position in the stack where
+     *                                        the token was found.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $next   = $phpcsFile->findNext(T_STYLE, ($stackPtr + 1));
+        if ($next === false) {
+            return;
+        }
+
+        if ($tokens[$next]['line'] === $tokens[$stackPtr]['line']) {
+            $error = 'Each style definition must be on a line by itself';
+            $phpcsFile->addError($error, $next);
+        }
+
+    }//end process()
+
+}//end class
+?>
