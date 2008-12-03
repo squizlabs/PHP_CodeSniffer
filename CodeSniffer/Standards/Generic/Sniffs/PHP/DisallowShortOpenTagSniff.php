0d88b226edbdd417bd260c50ec4794eb815bc40f
Greg Sherwood <gsherwood@squiz.net>
2008-12-03 04:42:41 +0000

    Coding standard fix
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@270343 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/Generic/Sniffs/PHP/DisallowShortOpenTagSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/PHP/DisallowShortOpenTagSniff.php
@@ -51,8 +51,8 @@ class Generic_Sniffs_PHP_DisallowShortOpenTagSniff implements PHP_CodeSniffer_Sn
      * Processes this test, when one of its tokens is encountered.
      *
      * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the
-     *                                        stack passed in $tokens.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
      *
      * @return void
      */
