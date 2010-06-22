b00eb3824c59daee2fa3884628c63648387cf26a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-06-22 08:25:27 +0000

    Close #16694.
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@300653 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ CodeSniffer/Standards/Generic/Sniffs/PHP/DeprecatedFunctionsSniff.php
@@ -0,0 +1,64 @@
+
+<?php
+/**
+ * Generic_Sniffs_PHP_DeprecatedFunctionsSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Generic_Sniffs_PHP_DeprecatedFunctionsSniff.
+ *
+ * Discourages the use of deprecated functions that are kept in PHP for
+ * compatibility with older versions.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Sebastian Bergmann <sb@sebastian-bergmann.de>
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Generic_Sniffs_PHP_DeprecatedFunctionsSniff extends Generic_Sniffs_PHP_ForbiddenFunctionsSniff
+{
+    /**
+     * A list of forbidden functions with their alternatives.
+     *
+     * The value is NULL if no alternative exists. IE, the
+     * function should just not be used.
+     *
+     * @var array(string => string|null)
+     */
+    protected $forbiddenFunctions = array();
+
+    /**
+     * Constructor.
+     *
+     * Uses the Reflection API to get a list of deprecated functions.
+     */
+    public function __construct()
+    {
+        $functions = get_defined_functions();
+
+        foreach ($functions['internal'] as $functionName) {
+            $function = new ReflectionFunction($functionName);
+
+            if ($function->isDeprecated()) {
+                $this->forbiddenFunctions[$functionName] = NULL;
+            }
+        }
+    }
+}
+?>
