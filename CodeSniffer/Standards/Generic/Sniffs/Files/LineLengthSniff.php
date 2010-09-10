7c32c14b78479f963ff576ca25ee7581c36d527c
Greg Sherwood <gsherwood@squiz.net>
2010-09-10 00:40:27 +0000

    Generic FileLengthSniff now uses iconv_strlen to check line length if an encoding is specified (request #14237)
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@303246 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/Generic/Sniffs/Files/LineLengthSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/Files/LineLengthSniff.php
@@ -66,8 +66,8 @@ class Generic_Sniffs_Files_LineLengthSniff implements PHP_CodeSniffer_Sniff
      * Processes this test, when one of its tokens is encountered.
      *
      * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the
-     *                                        stack passed in $tokens.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
      *
      * @return void
      */
@@ -118,7 +118,17 @@ class Generic_Sniffs_Files_LineLengthSniff implements PHP_CodeSniffer_Sniff
         if (preg_match('|@version[^\$]+\$Id|', $lineContent) === 0
             && preg_match('|@license|', $lineContent) === 0
         ) {
-            $lineLength = strlen($lineContent);
+            if (PHP_CODESNIFFER_ENCODING !== 'iso-8859-1') {
+                // Not using the detault encoding, so take a bit more care.
+                $lineLength = iconv_strlen($lineContent, PHP_CODESNIFFER_ENCODING);
+                if ($lineLength === false) {
+                    // String contained invalid characters, so revert to default.
+                    $lineLength = strlen($lineContent);
+                }
+            } else {
+               $lineLength = strlen($lineContent);
+            }
+
             if ($this->absoluteLineLimit > 0
                 && $lineLength > $this->absoluteLineLimit
             ) {
