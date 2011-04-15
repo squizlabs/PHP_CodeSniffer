684c92cad463fc87c50d8dd86fdc4736005081e3
Greg Sherwood <gsherwood@squiz.net>
2011-04-15 04:05:48 +0000

    Coding standards fix
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_CodeSniffer/trunk@310225 c90b9560-bf6c-de11-be94-00142212c4b1

--- CodeSniffer/Standards/Generic/Sniffs/Files/LineEndingsSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/Files/LineEndingsSniff.php
@@ -66,8 +66,8 @@ class Generic_Sniffs_Files_LineEndingsSniff implements PHP_CodeSniffer_Sniff
      * Processes this sniff, when one of its tokens is encountered.
      *
      * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the
-     *                                        stack passed in $tokens.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
      *
      * @return void
      */
