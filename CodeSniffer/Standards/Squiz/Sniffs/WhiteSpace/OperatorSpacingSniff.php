b32e6d8362a659e35f0f6926063548b85371a324
Greg Sherwood <gsherwood@squiz.net>
2012-07-23 13:22:36 +1000

    Squiz OperatorSpacingSniff no longer throws an error for negative values inside an inline THEN statement

--- CodeSniffer/Standards/Squiz/Sniffs/WhiteSpace/OperatorSpacingSniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/WhiteSpace/OperatorSpacingSniff.php
@@ -156,6 +156,7 @@ class Squiz_Sniffs_WhiteSpace_OperatorSpacingSniff implements PHP_CodeSniffer_Sn
                               T_OPEN_SQUARE_BRACKET,
                               T_DOUBLE_ARROW,
                               T_COLON,
+                              T_INLINE_THEN,
                              );
 
             if (in_array($tokens[$prev]['code'], $invalidTokens) === true) {
