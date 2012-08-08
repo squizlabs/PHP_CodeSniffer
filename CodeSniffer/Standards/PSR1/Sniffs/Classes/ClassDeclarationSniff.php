ce33009d73463e8c121ef8bf4387b1b969a1b1ee
Greg Sherwood <gsherwood@squiz.net>
2012-08-08 12:09:39 +1000

    Added sniff for the PSR-2 class checks

---
+++ CodeSniffer/Standards/PSR1/Sniffs/Classes/ClassDeclarationSniff.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Class Declaration Test.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006-2011 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * Class Declaration Test.
+ *
+ * Checks the declaration of the class is correct.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006-2011 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PSR1_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_CLASS,
+                T_INTERFACE,
+               );
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param integer              $stackPtr  The position of the current token in
+     *                                        the token stack.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens    = $phpcsFile->getTokens();
+        $nextClass = $phpcsFile->findNext(array(T_CLASS, T_INTERFACE), ($stackPtr + 1));
+        if ($nextClass !== false) {
+            $error = 'Each class must be in a file by itself';
+            $phpcsFile->addError($error, $nextClass, 'MultipleClasses');
+        }
+
+        $namespace = $phpcsFile->findPrevious(T_NAMESPACE, ($stackPtr - 1));
+        if ($namespace === false) {
+            $error = 'Each class must be in a namespace of at least one level (a top-level vendor name)';
+            $phpcsFile->addError($error, $stackPtr, 'MissingNamespace');
+        }
+
+    }//end process()
+
+
+}//end class
+
+?>
