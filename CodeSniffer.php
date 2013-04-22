697f4828b7e8afa06e1448c987216b50cd39b71a
Greg Sherwood <gsherwood@squiz.net>
2013-04-22 14:44:12 +1000

    Changelog + coding standards

--- CodeSniffer.php
+++ CodeSniffer.php
@@ -730,7 +730,7 @@ class PHP_CodeSniffer
         $excludedSniffs = array();
 
         if (is_dir($dir) === true) {
-            // available since PHP 5.2.11 and 5.3.1
+            // Available since PHP 5.2.11 and 5.3.1.
             if (defined('RecursiveDirectoryIterator::FOLLOW_SYMLINKS') === true) {
                 $di = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::FOLLOW_SYMLINKS));
             } else {
