363cadec12b11583a10293dbd463db8fe60c54ed
Greg Sherwood <gsherwood@squiz.net>
2013-06-19 16:48:01 +1000

    Fixed bug #19968 : Permission denied on excluded directory

--- CodeSniffer.php
+++ CodeSniffer.php
@@ -733,10 +733,12 @@ class PHP_CodeSniffer
         if (is_dir($dir) === true) {
             // Available since PHP 5.2.11 and 5.3.1.
             if (defined('RecursiveDirectoryIterator::FOLLOW_SYMLINKS') === true) {
-                $di = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::FOLLOW_SYMLINKS));
+                $rdi = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::FOLLOW_SYMLINKS);
             } else {
-                $di = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+                $rdi = new RecursiveDirectoryIterator($dir);
             }
+
+            $di = new RecursiveIteratorIterator($rdi, 0, RecursiveIteratorIterator::CATCH_GET_CHILD);
 
             foreach ($di as $file) {
                 $fileName = $file->getFilename();
@@ -1136,7 +1138,11 @@ class PHP_CodeSniffer
                 if ($local === true) {
                     $di = new DirectoryIterator($path);
                 } else {
-                    $di = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+                    $di = new RecursiveIteratorIterator(
+                        new RecursiveDirectoryIterator($path),
+                        0,
+                        RecursiveIteratorIterator::CATCH_GET_CHILD
+                    );
                 }
 
                 foreach ($di as $file) {
