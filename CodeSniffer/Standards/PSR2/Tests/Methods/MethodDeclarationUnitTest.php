33a791a425d11f3eadc4e1de8a79d6303b3b5c38
Renat Akhmedyanov <renat@topface.com>
2013-09-23 14:49:28 +0400

    Added method visibility declaration check.

--- CodeSniffer/Standards/PSR2/Tests/Methods/MethodDeclarationUnitTest.php
+++ CodeSniffer/Standards/PSR2/Tests/Methods/MethodDeclarationUnitTest.php
@@ -41,10 +41,13 @@ class PSR2_Tests_Methods_MethodDeclarationUnitTest extends AbstractSniffUnitTest
     public function getErrorList()
     {
         return array(
+                5  => 1,
+                7  => 1,
                 9  => 1,
                 11 => 1,
                 13 => 1,
                 15 => 3,
+                17 => 1,
                );
 
     }//end getErrorList()
