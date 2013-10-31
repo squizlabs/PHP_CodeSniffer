6ffb26634af356821e678c634ddaca91000be116
Greg Sherwood <gsherwood@squiz.net>
2013-10-31 12:07:50 +1100

    Coding standards fix

--- CodeSniffer/Standards/Generic/Sniffs/NamingConventions/UpperCaseConstantNameSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/NamingConventions/UpperCaseConstantNameSniff.php
@@ -106,7 +106,7 @@ class Generic_Sniffs_NamingConventions_UpperCaseConstantNameSniff implements PHP
             }
 
             return;
-        }
+        }//end if
 
         if (strtolower($constName) !== 'define') {
             return;
