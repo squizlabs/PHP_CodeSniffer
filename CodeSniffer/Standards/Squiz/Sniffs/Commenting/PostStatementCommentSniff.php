a097e81250b2d362b9751aa5ee63f317a1d56f5f
Greg Sherwood <gsherwood@squiz.net>
2013-12-02 15:23:09 +1100

    Added a fix for this error

--- CodeSniffer/Standards/Squiz/Sniffs/Commenting/PostStatementCommentSniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/Commenting/PostStatementCommentSniff.php
@@ -91,8 +91,11 @@ class Squiz_Sniffs_Commenting_PostStatementCommentSniff implements PHP_CodeSniff
             }
         }
 
-        $error = 'Comments may not appear after statements.';
-        $phpcsFile->addError($error, $stackPtr, 'Found');
+        $error = 'Comments may not appear after statements';
+        $fix = $phpcsFile->addFixableError($error, $stackPtr, 'Found');
+        if ($fix === true && $phpcsFile->fixer->enabled === true) {
+            $phpcsFile->fixer->addNewlineBefore($stackPtr);
+        }
 
     }//end process()
 
