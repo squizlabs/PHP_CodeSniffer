5c5eb51322c1ebea8188b1549ec0cf42a4945e66
Greg Sherwood <gsherwood@squiz.net>
2014-01-16 11:13:30 +1100

    Generic FixmeSniff and TodoSniff now work correctly with accented characters

--- CodeSniffer/Standards/Generic/Sniffs/Commenting/FixmeSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/Commenting/FixmeSniff.php
@@ -68,12 +68,12 @@ class Generic_Sniffs_Commenting_FixmeSniff implements PHP_CodeSniffer_Sniff
 
         $content = $tokens[$stackPtr]['content'];
         $matches = array();
-        if (preg_match('|[^a-z]+fixme[^a-z]+(.*)|i', $content, $matches) !== 0) {
+        if (preg_match('/(?:\A|[^\p{L}]+)fixme([^\p{L}]+(.*)|\Z)/ui', $content, $matches) !== 0) {
             // Clear whitespace and some common characters not required at
             // the end of a fixme message to make the error more informative.
             $type         = 'CommentFound';
             $fixmeMessage = trim($matches[1]);
-            $fixmeMessage = trim($fixmeMessage, '[]().');
+            $fixmeMessage = trim($fixmeMessage, '-:[](). ');
             $error        = 'Comment refers to a FIXME task';
             $data         = array($fixmeMessage);
             if ($fixmeMessage !== '') {
--- CodeSniffer/Standards/Generic/Sniffs/Commenting/TodoSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/Commenting/TodoSniff.php
@@ -66,12 +66,12 @@ class Generic_Sniffs_Commenting_TodoSniff implements PHP_CodeSniffer_Sniff
 
         $content = $tokens[$stackPtr]['content'];
         $matches = array();
-        if (preg_match('|[^a-z]+todo[^a-z]+(.*)|i', $content, $matches) !== 0) {
+        if (preg_match('/(?:\A|[^\p{L}]+)todo([^\p{L}]+(.*)|\Z)/ui', $content, $matches) !== 0) {
             // Clear whitespace and some common characters not required at
             // the end of a to-do message to make the warning more informative.
             $type        = 'CommentFound';
             $todoMessage = trim($matches[1]);
-            $todoMessage = trim($todoMessage, '[]().');
+            $todoMessage = trim($todoMessage, '-:[](). ');
             $error       = 'Comment refers to a TODO task';
             $data        = array($todoMessage);
             if ($todoMessage !== '') {
