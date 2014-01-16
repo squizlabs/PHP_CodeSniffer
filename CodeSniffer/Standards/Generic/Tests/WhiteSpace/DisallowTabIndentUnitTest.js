5a5262df9eab48e77a9ff179c521f62fa192ab41
chr0n1x <heilong24@gmail.com>
2014-01-16 15:56:19 -0500

    Removing all the things

--- CodeSniffer/Standards/Generic/Tests/WhiteSpace/DisallowTabIndentUnitTest.js
+++
@@ -1,9 +0,0 @@
-var x = {
-  abc: 1,
-	zyz: 2,
-  abc: 5,
-  mno: {
-  	abc: 4
-  },
-  abc: 5
-}
--- CodeSniffer/Standards/MySource/Tests/Debug/FirebugConsoleUnitTest.js
+++
@@ -1,8 +0,0 @@
-console.info();
-console.warn();
-console.test();
-con.sole();
-var console = {
-    console: 'string';
-};
-function console() {}
