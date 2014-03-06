8505439fa139c4853b140871e3e14e2004883ab9
Greg Sherwood <gsherwood@squiz.net>
2014-03-06 15:54:56 +1100

    PSR1 and PEAR ClassDeclarationSniffs now support traits (request #20208)

--- CodeSniffer/Standards/PSR1/Sniffs/Classes/ClassDeclarationSniff.php
+++ CodeSniffer/Standards/PSR1/Sniffs/Classes/ClassDeclarationSniff.php
@@ -39,6 +39,7 @@ class PSR1_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniffer_Sniff
         return array(
                 T_CLASS,
                 T_INTERFACE,
+                T_TRAIT,
                );
 
     }//end register()
@@ -56,17 +57,19 @@ class PSR1_Sniffs_Classes_ClassDeclarationSniff implements PHP_CodeSniffer_Sniff
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
         $tokens    = $phpcsFile->getTokens();
-        $nextClass = $phpcsFile->findNext(array(T_CLASS, T_INTERFACE), ($stackPtr + 1));
+        $errorData = array(strtolower($tokens[$stackPtr]['content']));
+
+        $nextClass = $phpcsFile->findNext(array(T_CLASS, T_INTERFACE, T_TRAIT), ($stackPtr + 1));
         if ($nextClass !== false) {
-            $error = 'Each class must be in a file by itself';
-            $phpcsFile->addError($error, $nextClass, 'MultipleClasses');
+            $error = 'Each %s must be in a file by itself';
+            $phpcsFile->addError($error, $nextClass, 'MultipleClasses', $errorData);
         }
 
         if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
             $namespace = $phpcsFile->findPrevious(T_NAMESPACE, ($stackPtr - 1));
             if ($namespace === false) {
-                $error = 'Each class must be in a namespace of at least one level (a top-level vendor name)';
-                $phpcsFile->addError($error, $stackPtr, 'MissingNamespace');
+                $error = 'Each %s must be in a namespace of at least one level (a top-level vendor name)';
+                $phpcsFile->addError($error, $stackPtr, 'MissingNamespace', $errorData);
             }
         }
 
