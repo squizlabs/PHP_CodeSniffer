d04c71c41a4ade139e91621636450ead755ac502
Greg Sherwood <gsherwood@squiz.net>
2014-06-24 15:19:25 +1000

    Squiz InlineCommentSniff no longer requires a blank line after post-statement comments (request #20299)

--- CodeSniffer/Standards/Squiz/Sniffs/Commenting/InlineCommentSniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/Commenting/InlineCommentSniff.php
@@ -124,14 +124,14 @@ class Squiz_Sniffs_Commenting_InlineCommentSniff implements PHP_CodeSniffer_Snif
 
                 // Only error once per comment.
                 if (substr($tokens[$stackPtr]['content'], 0, 3) === '/**') {
-                    $error  = 'Inline doc block comments are not allowed; use "/* Comment */" or "// Comment" instead';
+                    $error = 'Inline doc block comments are not allowed; use "/* Comment */" or "// Comment" instead';
                     $phpcsFile->addError($error, $stackPtr, 'DocBlock');
                 }
             }//end if
         }//end if
 
         if ($tokens[$stackPtr]['content']{0} === '#') {
-            $error  = 'Perl-style comments are not allowed; use "// Comment" instead';
+            $error = 'Perl-style comments are not allowed; use "// Comment" instead';
             $phpcsFile->addError($error, $stackPtr, 'WrongStyle');
         }
 
@@ -248,25 +248,25 @@ class Squiz_Sniffs_Commenting_InlineCommentSniff implements PHP_CodeSniffer_Snif
             $phpcsFile->addError($error, $stackPtr, 'InvalidEndChar', $data);
         }
 
-        // Finally, the line below the last comment cannot be empty.
-        $start = false;
-        for ($i = ($stackPtr + 1); $i < $phpcsFile->numTokens; $i++) {
-            if ($tokens[$i]['line'] === ($tokens[$stackPtr]['line'] + 1)) {
-                if ($tokens[$i]['code'] !== T_WHITESPACE) {
-                    return;
+        // Finally, the line below the last comment cannot be empty if this inline
+        // comment is on a line by itself.
+        if ($tokens[$previousContent]['line'] < $tokens[$stackPtr]['line']) {
+            $start = false;
+            for ($i = ($stackPtr + 1); $i < $phpcsFile->numTokens; $i++) {
+                if ($tokens[$i]['line'] === ($tokens[$stackPtr]['line'] + 1)) {
+                    if ($tokens[$i]['code'] !== T_WHITESPACE) {
+                        return;
+                  }
+                } else if ($tokens[$i]['line'] > ($tokens[$stackPtr]['line'] + 1)) {
+                    break;
                 }
-            } else if ($tokens[$i]['line'] > ($tokens[$stackPtr]['line'] + 1)) {
-                break;
             }
-        }
 
-        $error = 'There must be no blank line following an inline comment';
-        $phpcsFile->addError($error, $stackPtr, 'SpacingAfter');
+            $error = 'There must be no blank line following an inline comment';
+            $phpcsFile->addError($error, $stackPtr, 'SpacingAfter');
+        }
 
     }//end process()
 
 
 }//end class
-
-
-?>
