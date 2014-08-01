68cb22bb8a28e2ce6b244a0a2c0b2e17f3351c46
Jonathan Marcil <jonathan.marcil@gmail.com>
2014-08-01 14:01:04 -0400

    small fix for makeFriendlyName() when the name finish with a cap it raised an exception

--- CodeSniffer/Reports/Source.php
+++ CodeSniffer/Reports/Source.php
@@ -208,10 +208,12 @@ class PHP_CodeSniffer_Reports_Source implements PHP_CodeSniffer_Report
                     // Uppercase.
                     if ($lastWasUpper === false) {
                         $friendlyName .= ' ';
-                        $next = $name[($i + 1)];
-                        if (strtolower($next) === $next) {
-                            // Next char is lowercase so it is a word boundary.
-                            $name[$i] = strtolower($name[$i]);
+                        if ($i < ($length - 1)) {
+                            $next = $name[($i + 1)];
+                            if (strtolower($next) === $next) {
+                                // Next char is lowercase so it is a word boundary.
+                                $name[$i] = strtolower($name[$i]);
+                            }
                         }
                     }
 
