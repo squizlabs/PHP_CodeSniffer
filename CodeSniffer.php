8d303cfac5d65917a4194a9248c74c9b179464f9
Greg Sherwood <gsherwood@squiz.net>
2014-11-10 15:40:58 +1100

    Fixed changelog and current version number

--- CodeSniffer.php
+++ CodeSniffer.php
@@ -73,7 +73,7 @@ class PHP_CodeSniffer
      *
      * @var string
      */
-    const VERSION = '2.0.0RC4';
+    const VERSION = '2.0.0RC5';
 
     /**
      * Package stability; either stable, beta or alpha.
