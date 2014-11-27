6b88f8cd270178715fc86173acb59283e8f1ad5e
Greg Sherwood <gsherwood@squiz.net>
2014-11-28 09:17:32 +1100

    PEAR MultiLineConditionSniff now supports JS files

--- CodeSniffer/Standards/PEAR/Sniffs/ControlStructures/MultiLineConditionSniff.php
+++ CodeSniffer/Standards/PEAR/Sniffs/ControlStructures/MultiLineConditionSniff.php
@@ -29,6 +29,16 @@ class PEAR_Sniffs_ControlStructures_MultiLineConditionSniff implements PHP_CodeS
 {
 
     /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+                                   'PHP',
+                                   'JS',
+                                  );
+
+    /**
      * The number of spaces code should be indented.
      *
      * @var int
