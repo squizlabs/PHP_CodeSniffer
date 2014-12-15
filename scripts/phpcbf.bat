4a5c496f929146b0eebf13b0cc5a105bcf559755
Greg Sherwood <gsherwood@squiz.net>
2014-12-15 15:59:47 +1100

    Fixed bug #346 : PHP path can't be determined in some cases in phpcs.bat (on Windows XP)

--- scripts/phpcbf.bat
+++ scripts/phpcbf.bat
@@ -10,4 +10,10 @@ REM @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
 REM @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
 REM @link      http://pear.php.net/package/PHP_CodeSniffer
 
-"@php_bin@" -d auto_append_file="" -d auto_prepend_file="" -d include_path="'@php_dir@'" -f "@bin_dir@\phpcbf" -- %*
+if "%PHPBIN%" == "" set PHPBIN=@php_bin@
+if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
+GOTO RUN
+:USE_PEAR_PATH
+set PHPBIN=%PHP_PEAR_PHP_BIN%
+:RUN
+"%PHPBIN%" "@bin_dir@\phpcbf" %*
