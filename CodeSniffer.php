cc0a8127cbe9d431e83cb6f6312ea3969fc0935b
Greg Sherwood <gsherwood@squiz.net>
2015-03-04 13:06:48 +1100

    Prepare for 2.3.0 release

--- CodeSniffer.php
+++ CodeSniffer.php
@@ -73,7 +73,7 @@ class PHP_CodeSniffer
      *
      * @var string
      */
-    const VERSION = '2.2.1';
+    const VERSION = '2.3.0';
 
     /**
      * Package stability; either stable, beta or alpha.
