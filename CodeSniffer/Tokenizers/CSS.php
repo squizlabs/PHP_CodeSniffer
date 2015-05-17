d946240b325f7c601f33257aba500c59fee21d61
Klaus Purer <klaus.purer@gmail.com>
2015-05-17 02:35:54 +0200

    Improved CSS tokenizer to work with At-rules like @media queries

--- CodeSniffer/Tokenizers/CSS.php
+++ CodeSniffer/Tokenizers/CSS.php
@@ -249,9 +249,12 @@ class PHP_CodeSniffer_Tokenizers_CSS extends PHP_CodeSniffer_Tokenizers_PHP
         }//end for
 
         // A flag to indicate if we are inside a style definition,
-        // which is defined using curly braces. I'm assuming you can't
-        // have nested curly brackets.
+        // which is defined using curly braces.
         $inStyleDef = false;
+
+        // A flag to indicate if an At-rule like "@media" is used, which will result
+        // in nested curly brackets.
+        $asperandStart = false;
 
         $numTokens = count($finalTokens);
         for ($stackPtr = 0; $stackPtr < $numTokens; $stackPtr++) {
@@ -259,10 +262,20 @@ class PHP_CodeSniffer_Tokenizers_CSS extends PHP_CodeSniffer_Tokenizers_PHP
 
             switch ($token['code']) {
             case T_OPEN_CURLY_BRACKET:
-                $inStyleDef = true;
+                // Opening curly brackets for an At-rule do not start a style
+                // definition. We also reset the asperand flag here because the next
+                // opening curly bracket could be indeed the start of a style
+                // definition.
+                if ($asperandStart === true) {
+                    $inStyleDef    = false;
+                    $asperandStart = false;
+                } else {
+                    $inStyleDef = true;
+                }
                 break;
             case T_CLOSE_CURLY_BRACKET:
-                $inStyleDef = false;
+                $inStyleDef    = false;
+                $asperandStart = false;
                 break;
             case T_MINUS:
                 // Minus signs are often used instead of spaces inside
@@ -354,6 +367,9 @@ class PHP_CodeSniffer_Tokenizers_CSS extends PHP_CodeSniffer_Tokenizers_PHP
                     }
                 }//end if
 
+                break;
+            case T_ASPERAND:
+                $asperandStart = true;
                 break;
             default:
                 // Nothing special to be done with this token.
