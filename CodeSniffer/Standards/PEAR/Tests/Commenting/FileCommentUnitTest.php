aa6a986a32cb83bb647aa518610a4928c7248ba9
Greg Sherwood <gsherwood@squiz.net>
2015-10-07 08:38:52 +1100

    Fixed bug #732 : PHP Notice if @package name is made up of all invalid characters

--- CodeSniffer/Standards/PEAR/Tests/Commenting/FileCommentUnitTest.php
+++ CodeSniffer/Standards/PEAR/Tests/Commenting/FileCommentUnitTest.php
@@ -63,7 +63,9 @@ class PEAR_Tests_Commenting_FileCommentUnitTest extends AbstractSniffUnitTest
                 33 => 1,
                 34 => 1,
                 35 => 1,
-                40 => 1,
+                40 => 2,
+                41 => 2,
+                42 => 1,
                );
 
     }//end getErrorList()
@@ -83,7 +85,7 @@ class PEAR_Tests_Commenting_FileCommentUnitTest extends AbstractSniffUnitTest
                 29 => 1,
                 30 => 1,
                 34 => 1,
-                40 => 1,
+                42 => 1,
                );
 
     }//end getWarningList()
