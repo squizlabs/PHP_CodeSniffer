ed257ca0e56ad86cd2a4d6fa38ce0b95141c824f
Greg Sherwood <gsherwood@squiz.net>
2015-12-17 16:37:50 +1100

    Fixed bug #820 : PEAR/PSR2 FunctionCallSignature sniffs suggest wrong indent when there are multiple arguments on a line

--- CodeSniffer/Standards/PSR2/Tests/Methods/FunctionCallSignatureUnitTest.php
+++ CodeSniffer/Standards/PSR2/Tests/Methods/FunctionCallSignatureUnitTest.php
@@ -43,12 +43,14 @@ class PSR2_Tests_Methods_FunctionCallSignatureUnitTest extends AbstractSniffUnit
     public function getErrorList()
     {
         return array(
-                18 => 3,
-                21 => 1,
-                48 => 1,
-                87 => 1,
-                90 => 1,
-                91 => 1,
+                18  => 3,
+                21  => 1,
+                48  => 1,
+                87  => 1,
+                90  => 1,
+                91  => 1,
+                103 => 1,
+                111 => 1,
                );
 
     }//end getErrorList()
