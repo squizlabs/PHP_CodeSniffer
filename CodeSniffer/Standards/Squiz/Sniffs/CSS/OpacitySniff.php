7d57fba070cae4dba0da5f434c4129d00ac3f9c0
Greg Sherwood <gsherwood@squiz.net>
2016-01-21 13:51:03 +1100

    Renamed Squiz.CSS.Opacity.SpacingAfterPoint to Squiz.CSS.Opacity.DecimalPrecision (ref #871)

--- CodeSniffer/Standards/Squiz/Sniffs/CSS/OpacitySniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/CSS/OpacitySniff.php
@@ -83,7 +83,7 @@ class Squiz_Sniffs_CSS_OpacitySniff implements PHP_CodeSniffer_Sniff
         } else {
             if (strlen($value) > 3) {
                 $error = 'Opacity values must have a single value after the decimal point';
-                $phpcsFile->addError($error, $next, 'SpacingAfterPoint');
+                $phpcsFile->addError($error, $next, 'DecimalPrecision');
             } else if ($value === '0.0' || $value === '1.0') {
                 $error = 'Opacity value does not require decimal point; use %s instead';
                 $data  = array($value{0});
