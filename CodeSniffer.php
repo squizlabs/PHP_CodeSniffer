fa4e2afb6d15c52af03ca70bb31a74fd56120115
Pieter Frenssen <pieter@frenssen.be>
2016-02-15 16:40:08 +0100

    Support the 'rulesetdir' directive in phpcs-ruleset.xml.

--- CodeSniffer.php
+++ CodeSniffer.php
@@ -704,6 +704,11 @@ class PHP_CodeSniffer
         $excludedSniffs = array();
         $cliValues      = $this->cli->getCommandLineValues();
 
+        // Populate the list of ruleset directories.
+        foreach ($ruleset->rulesetdir as $dir) {
+            self::$rulesetDirs[] = (string) $dir['dir'];
+        }
+
         $rulesetDir          = dirname($rulesetPath);
         $rulesetName         = basename($rulesetPath);
         self::$rulesetDirs[] = $rulesetDir;
