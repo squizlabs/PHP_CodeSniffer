bb43d8d52de3f73a941007a5a7c48b42e1944422
Walt Sorensen <photodude@users.noreply.github.com>
2016-04-24 22:26:26 -0600

    Option exception to the newline requirement on the first member var after class opener.
    Based on the draft PHPCS2 Joomla Custom member var sniff https://github.com/photodude/coding-standards/blob/phpcs-2/Joomla/Sniffs/WhiteSpace/MemberVarSpacingSniff.php#L102-L137

--- CodeSniffer/Standards/Squiz/Sniffs/WhiteSpace/MemberVarSpacingSniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/WhiteSpace/MemberVarSpacingSniff.php
@@ -34,6 +34,15 @@ class Squiz_Sniffs_WhiteSpace_MemberVarSpacingSniff extends PHP_CodeSniffer_Stan
 
 
     /**
+     * The exception to the newline requirement on the first member var after class opener.
+     * false for newline requirement on the first member var after class opener
+     * true for no newline requirement on the first member var after class opener
+     *
+     * @var bool
+     */
+    public $firstVarException = false;
+
+    /**
      * Processes the function tokens within the class.
      *
      * @param PHP_CodeSniffer_File $phpcsFile The file where this token was found.
@@ -96,9 +105,38 @@ class Squiz_Sniffs_WhiteSpace_MemberVarSpacingSniff extends PHP_CodeSniffer_Stan
 
         $prev       = $phpcsFile->findPrevious(PHP_CodeSniffer_Tokens::$emptyTokens, ($first - 1), null, true);
         $foundLines = ($tokens[$first]['line'] - $tokens[$prev]['line'] - 1);
-        if ($foundLines === 1) {
-            return;
-        }
+
+		// Exception to the newline requirement on the first member var after class opener.
+		if($this->firstVarException)
+		{
+    		if ($foundLines > 0 && $tokens[$prev]['code'] === T_OPEN_CURLY_BRACKET)
+    		{
+    			$error = 'Expected 0 blank lines before first member var; %s found';
+    			$data  = array($foundLines);
+    			$fix   = $phpcsFile->addFixableError($error, $stackPtr, 'FirstMember', $data);
+    			if ($fix === true)
+    			{
+    				$phpcsFile->fixer->beginChangeset();
+    				for ($i = ($prev + 1); $i < $first; $i++)
+    				{
+    					if ($tokens[$i]['line'] === $tokens[$prev]['line'])
+    					{
+    						continue;
+    					}
+    					if ($tokens[$i]['line'] === $tokens[$first]['line'])
+    					{
+    						break;
+    					}
+    					$phpcsFile->fixer->replaceToken($i, '');
+    				}
+    				$phpcsFile->fixer->endChangeset();
+    			}
+    		}
+		}
+		if ($foundLines === 1 || ($this->firstVarException && $tokens[$prev]['code'] === T_OPEN_CURLY_BRACKET))
+		{
+			return;
+		}
 
         $error = 'Expected 1 blank line before member var; %s found';
         $data  = array($foundLines);
