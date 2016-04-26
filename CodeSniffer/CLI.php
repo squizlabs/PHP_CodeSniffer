3175b713d51449da190effbececccbc9112b3cba
Greg Sherwood <gsherwood@squiz.net>
2016-04-27 09:56:39 +1000

    Installations done via composer now only include the composer autoloader for PHP 5.3.2+ (request #942)

--- CodeSniffer/CLI.php
+++ CodeSniffer/CLI.php
@@ -14,11 +14,13 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-// Installations via Composer: make sure that we autoload all dependencies.
-if (file_exists($a = dirname(__FILE__).'/../../../autoload.php') === true) {
-    include_once $a;
-} else if (file_exists($a = dirname(__FILE__).'/../vendor/autoload.php') === true) {
-    include_once $a;
+// Make sure that we autoload all dependencies if running via Composer.
+if (version_compare(PHP_VERSION, '5.3.2', '>=') === true) {
+    if (file_exists($a = dirname(__FILE__).'/../../../autoload.php') === true) {
+        include_once $a;
+    } else if (file_exists($a = dirname(__FILE__).'/../vendor/autoload.php') === true) {
+        include_once $a;
+    }
 }
 
 if (file_exists($a = dirname(__FILE__).'/../CodeSniffer.php') === true) {
@@ -243,7 +245,7 @@ class PHP_CodeSniffer_CLI
     public function checkRequirements()
     {
         // Check the PHP version.
-        if (version_compare(PHP_VERSION, '5.1.2') === -1) {
+        if (version_compare(PHP_VERSION, '5.1.2', '<') === true) {
             echo 'ERROR: PHP_CodeSniffer requires PHP version 5.1.2 or greater.'.PHP_EOL;
             exit(2);
         }
