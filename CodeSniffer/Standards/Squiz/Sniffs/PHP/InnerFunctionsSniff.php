e37a50c74cd1ef528de133be57d2d16ddf264b27
Greg Sherwood <gsherwood@squiz.net>
2016-04-26 10:57:09 +1000

    Fixed bug #943 : Squiz.PHP.InnerFunctions.NotAllowed reported in anonymous classes

--- CodeSniffer/Standards/Squiz/Sniffs/PHP/InnerFunctionsSniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/PHP/InnerFunctionsSniff.php
@@ -56,16 +56,26 @@ class Squiz_Sniffs_PHP_InnerFunctionsSniff implements PHP_CodeSniffer_Sniff
     {
         $tokens = $phpcsFile->getTokens();
 
-        if ($phpcsFile->hasCondition($stackPtr, T_FUNCTION) === true) {
-            $prev = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
-            if ($tokens[$prev]['code'] === T_EQUAL) {
-                // Ignore closures.
-                return;
-            }
-
-            $error = 'The use of inner functions is forbidden';
-            $phpcsFile->addError($error, $stackPtr, 'NotAllowed');
+        $function = $phpcsFile->getCondition($stackPtr, T_FUNCTION);
+        if ($function === false) {
+            // Not a nested function.
+            return;
         }
+
+        $class = $phpcsFile->getCondition($stackPtr, T_ANON_CLASS);
+        if ($class !== false && $class > $function) {
+            // Ignore methods in anon classes.
+            return;
+        }
+
+        $prev = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
+        if ($tokens[$prev]['code'] === T_EQUAL) {
+            // Ignore closures.
+            return;
+        }
+
+        $error = 'The use of inner functions is forbidden';
+        $phpcsFile->addError($error, $stackPtr, 'NotAllowed');
 
     }//end process()
 
