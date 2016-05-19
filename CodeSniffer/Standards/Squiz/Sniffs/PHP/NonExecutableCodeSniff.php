87f99f7e25538530010ec1d1d7746c18da3d2d9d
Derek Henderson <derek.henderson@remote-learner.net>
2016-05-19 07:51:24 -0400

    PHP-1010 Correct problem with NonExecutatable code sniff not detecting boolean or (issue 1010).

--- CodeSniffer/Standards/Squiz/Sniffs/PHP/NonExecutableCodeSniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/PHP/NonExecutableCodeSniff.php
@@ -66,7 +66,7 @@ class Squiz_Sniffs_PHP_NonExecutableCodeSniff implements PHP_CodeSniffer_Sniff
         // If this token is preceded with an "or", it only relates to one line
         // and should be ignored. For example: fopen() or die().
         $prev = $phpcsFile->findPrevious(PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr - 1), null, true);
-        if ($tokens[$prev]['code'] === T_LOGICAL_OR) {
+        if ($tokens[$prev]['code'] === T_LOGICAL_OR || $tokens[$prev]['code'] === T_BOOLEAN_OR) {
             return;
         }
 
