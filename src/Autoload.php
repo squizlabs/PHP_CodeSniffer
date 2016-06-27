710868393dd56f06ddb5b3d0c9e2fb8be95f5e67
TomasVotruba <tomas.vot@gmail.com>
2016-06-27 02:58:16 +0200

    use origin package for sniffs, move autoload to namespace

--- src/Autoload.php
+++ src/Autoload.php
@@ -51,9 +51,9 @@ class Autoload
 
         if (substr($class, 0, 16) === 'PHP_CodeSniffer\\') {
             if (substr($class, 0, 22) === 'PHP_CodeSniffer\Tests\\') {
-                $path = __DIR__.$ds.'tests'.$ds.substr(str_replace('\\', $ds, $class), 22).'.php';
+                $path = __DIR__ . $ds .$ds.substr(str_replace('\\', $ds, $class), 22).'.php';
             } else {
-                $path = __DIR__.$ds.'src'.$ds.substr(str_replace('\\', $ds, $class), 16).'.php';
+                $path = __DIR__ . $ds .$ds.substr(str_replace('\\', $ds, $class), 16).'.php';
             }
         }
 
