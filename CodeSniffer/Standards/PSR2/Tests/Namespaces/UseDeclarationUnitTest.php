6906a9f2a828ef09311f27652c04d0f446c7e899
Jason McCreary <jason@pureconcepts.net>
2016-08-02 21:39:45 -0400

    "Ungroup" grouped use statements for PSR-2

--- CodeSniffer/Standards/PSR2/Tests/Namespaces/UseDeclarationUnitTest.php
+++ CodeSniffer/Standards/PSR2/Tests/Namespaces/UseDeclarationUnitTest.php
@@ -54,6 +54,11 @@ class PSR2_Tests_Namespaces_UseDeclarationUnitTest extends AbstractSniffUnitTest
                     4 => 1,
                     6 => 1,
                    );
+        case 'UseDeclarationUnitTest.5.inc':
+            return array(
+                5 => 1,
+                7 => 1,
+            );
         default:
             return array();
         }//end switch
