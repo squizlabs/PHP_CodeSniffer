6299417847d84e257a2430996cc22205d38a6109
Greg Sherwood <gsherwood@squiz.net>
2017-01-25 08:35:47 +1100

    Added more guard code for CSS files with syntax errors (request #1304)

--- CodeSniffer/Tokenizers/CSS.php
+++ CodeSniffer/Tokenizers/CSS.php
@@ -449,7 +449,9 @@ class PHP_CodeSniffer_Tokenizers_CSS extends PHP_CodeSniffer_Tokenizers_PHP
                 } else if ($finalTokens[$stackPtr]['content'][0] === '-'
                     && $finalTokens[($stackPtr + 1)]['code'] === T_STRING
                 ) {
-                    if ($finalTokens[($stackPtr - 1)]['code'] === T_STRING) {
+                    if (isset($finalTokens[($stackPtr - 1)]) === true
+                        && $finalTokens[($stackPtr - 1)]['code'] === T_STRING
+                    ) {
                         $newContent = $finalTokens[($stackPtr - 1)]['content'].$finalTokens[$stackPtr]['content'].$finalTokens[($stackPtr + 1)]['content'];
 
                         if (PHP_CODESNIFFER_VERBOSITY > 1) {
