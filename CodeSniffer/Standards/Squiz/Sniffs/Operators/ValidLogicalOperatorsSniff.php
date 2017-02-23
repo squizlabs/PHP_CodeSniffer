44792fdd888954b21668b0c324df825d3b58b35a
Don Koopa <koopzington@gmail.com>
2017-02-23 19:37:20 +0100

    Fix CS

--- CodeSniffer/Standards/Squiz/Sniffs/Operators/ValidLogicalOperatorsSniff.php
+++ CodeSniffer/Standards/Squiz/Sniffs/Operators/ValidLogicalOperatorsSniff.php
@@ -75,10 +75,11 @@ class Squiz_Sniffs_Operators_ValidLogicalOperatorsSniff implements PHP_CodeSniff
                   $operator,
                   $replacements[$operator],
                  );
-        $fix = $phpcsFile->addFixableError($error, $stackPtr, 'NotAllowed', $data);
+        $fix   = $phpcsFile->addFixableError($error, $stackPtr, 'NotAllowed', $data);
         if ($fix === true) {
             $phpcsFile->fixer->replaceToken($stackPtr, $replacements[$operator]);
         }
+
     }//end process()
 
 
