06bf7e90c929fdf5d125495a989f47d57b1a2395
Diogo Alexsander Cavilha <diogo@agenciasys.com.br>
2017-04-27 16:24:14 -0300

    Add white spaces rules
    - Spaces arround the assignment symbols.
    - Single space between type cast and variable

---
+++ CodeSniffer/Standards/PSR2/Sniffs/WhiteSpaces/AssignmentSniff.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * PSR2_Sniffs_WhiteSpace_AssignmentSpacingSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * PSR2_Sniffs_WhiteSpace_AssignmentSpacingSniff.
+ *
+ * Ensures the assignment operators are surrounded with whitespace.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Diogo Alexsander Cavilha <diogocavilha@gmail.com>
+ * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PSR2_Sniffs_WhiteSpaces_AssignmentSniff
+    implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return PHP_CodeSniffer_Tokens::$assignmentTokens;
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $tokenBefore = $tokens[$stackPtr -1]['code'];
+        $tokenAfter = $tokens[$stackPtr +1]['code'];
+
+        if (T_WHITESPACE !== $tokenBefore || T_WHITESPACE !== $tokenAfter) {
+            $phpcsFile->addError(
+                'A space was expected around the assignment operator. 0 found.',
+                $stackPtr,
+                'Invalid'
+            );
+        }
+    }
+}
