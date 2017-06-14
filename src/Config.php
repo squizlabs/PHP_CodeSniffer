fbf77fd91008d3eafe595faf7365e43bc162ad64
Greg Sherwood <gsherwood@squiz.net>
2017-06-15 08:47:39 +1000

    Fixed a problem where the --cache=/path/to/cachefile CLI argument was not working (ref #1507)

--- src/Config.php
+++ src/Config.php
@@ -815,7 +815,10 @@ class Config
             } else if (defined('PHP_CODESNIFFER_IN_TESTS') === false
                 && substr($arg, 0, 6) === 'cache='
             ) {
-                if ($this->cache === false || isset($this->overriddenDefaults['cacheFile']) === true) {
+                if (($this->overriddenDefaults['cache'] === true
+                    && $this->cache === false)
+                    || isset($this->overriddenDefaults['cacheFile']) === true
+                ) {
                     break;
                 }
 
