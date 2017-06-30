5c1078a18149fef6f056acbca1174eab1981bbc8
bondas83 <bondas83@gmail.com>
2017-06-30 19:40:16 +0300

    Update ObjectOperatorSpacingSniff.php
    Validate and Class :: staticFunction() and fix to Class::staticFunction() without spaces

--- src/Standards/Squiz/Sniffs/WhiteSpace/ObjectOperatorSpacingSniff.php
+++ src/Standards/Squiz/Sniffs/WhiteSpace/ObjectOperatorSpacingSniff.php
@@ -30,7 +30,7 @@ class ObjectOperatorSpacingSniff implements Sniff
      */
     public function register()
     {
-        return array(T_OBJECT_OPERATOR);
+        return array(T_OBJECT_OPERATOR, T_DOUBLE_COLON);
 
     }//end register()
 
