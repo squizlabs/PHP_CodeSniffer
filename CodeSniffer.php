393ffffb2b60142cfb7f8277b63f2ea3625a10c0
Greg Sherwood <gsherwood@squiz.net>
2017-07-13 09:35:51 +1000

    Changelog for #1549

--- CodeSniffer.php
+++ CodeSniffer.php
@@ -73,7 +73,7 @@ class PHP_CodeSniffer
      *
      * @var string
      */
-    const VERSION = '2.9.1';
+    const VERSION = '2.9.2';
 
     /**
      * Package stability; either stable, beta or alpha.
