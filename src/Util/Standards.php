squizlabs/PHP_CodeSniffer

commit d83770d843b3e32bd5f065e24740314c8747ccbd

:fire: Fix broken/incompatible `installed_paths` in PHPCS 3.x
If a "Standards" directory is called the same as one of the standards it contains and that standard is requested an _"ERROR: the ... coding standard is not installed."_ will be thrown and the sniffs will not run.

While I suppose this change in behaviour between PHPCS 3.x and PHPCS 2.x is intended to add support for "one standard" type of libraries, it breaks support for multi-standard libraries where one of the standards has the same name as the repository.

The change I'm proposing in this PR will maintain support for the PHPCS 2.x behaviour - thus not breaking existing registered standards - while at the same time supporting the new feature.

src/Util/Standards.php
@@ -164,10 +164,9 @@ class Standards
     {
         $installedPaths = self::getInstalledStandardPaths();
         foreach ($installedPaths as $installedPath) {
-            if (basename($installedPath) === $standard) {
+            $standardPath = $installedPath.DIRECTORY_SEPARATOR.$standard;
+            if (file_exists($standardPath) === false && basename($installedPath) === $standard) {
                 $standardPath = $installedPath;
-            } else {
-                $standardPath = $installedPath.DIRECTORY_SEPARATOR.$standard;
             }
 
             $path = Common::realpath($standardPath.DIRECTORY_SEPARATOR.'ruleset.xml');

commit 7cfadc68da6ff6446640c1b25d313b4ee4a01426

Hotfix for when one-standard directories are being registered in the `installed_paths`.
If any external standard or a custom ruleset refers to a complete standard and this standard is  registered in the `installed_paths` config as a one-standard directory, a `Reference sniff ...  does not exist` error will be thrown and no sniffs will be run.

Bug introduced in #1450 (by me, sorry!).

The reason for this is that the `getInstalledStandardPath()` method expects a string, but will  in actual fact receive a `SimpleXMLElement` object when a complete standard is included by  another standard. This was previously not documented in the function doc block as a possibility.

While in the original code, the `$standards` name was only used as a concatenated string - which  triggered the `SimpleXML.toString()` method -, the fix in #1450 introduced a strict comparison  without the `toString()` conversion, causing the bug.

Installed_paths | string `$standards` | SimpleXMLElement `$standards`
--------------- | ------------------- | ----------------------
"Standards" directory | :white_check_mark: | :white_check_mark:
one-standard directory | :white_check_mark: | :x: <= This is what this fixes.

While debugging this, I also noticed that this method is called for every single ref in the  ruleset and would go through the whole loop before returning `null`, while for `refs` which  refer to a custom xml ruleset or to a sniff or sniff category, `null` can be returned a lot  earlier and for paths which don't resolve to a ruleset, part of the loop can be skipped.

src/Util/Standards.php
@@ -255,16 +255,26 @@ class Standards
      * CodeSniffer/Standards directory. Valid coding standards
      * include a ruleset.xml file.
      *
-     * @param string $standard The name of the coding standard.
+     * @param string|\SimpleXMLElement $standard The name of the coding standard.
      *
      * @return string|null
      */
     public static function getInstalledStandardPath($standard)
     {
         $installedPaths = self::getInstalledStandardPaths();
+        $standard       = (string) $standard;
+
+        if (strpos($standard, '.') !== false) {
+            return null;
+        }
+
         foreach ($installedPaths as $installedPath) {
             $standardPath = $installedPath.DIRECTORY_SEPARATOR.$standard;
-            if (file_exists($standardPath) === false && basename($installedPath) === $standard) {
+            if (file_exists($standardPath) === false) {
+                if (basename($installedPath) !== (string) $standard) {
+                    continue;
+                }
+
                 $standardPath = $installedPath;
             }
 
@@ -278,7 +288,7 @@ class Standards
                     return $path;
                 }
             }
-        }
+        }//end foreach
 
         return null;
 