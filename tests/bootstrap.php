16ace84ad83901558d79f9232cfe3b9511d7225e
Vytautas Stankus <svycka@users.noreply.github.com>
2017-10-05 11:26:58 +0300

    let's ignore workaround

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -30,17 +30,21 @@ $tokens = new \PHP_CodeSniffer\Util\Tokens();
 
 // Compatibility for PHPUnit < 6 and PHPUnit 6+.
 if (class_exists('PHPUnit_Framework_TestSuite') === true && class_exists('PHPUnit\Framework\TestSuite') === false) {
+    // @codingStandardsIgnoreLine
     class_alias('PHPUnit_Framework_TestSuite', 'PHPUnit'.'\Framework\TestSuite');
 }
 
 if (class_exists('PHPUnit_Framework_TestCase') === true && class_exists('PHPUnit\Framework\TestCase') === false) {
+    // @codingStandardsIgnoreLine
     class_alias('PHPUnit_Framework_TestCase', 'PHPUnit'.'\Framework\TestCase');
 }
 
 if (class_exists('PHPUnit_TextUI_TestRunner') === true && class_exists('PHPUnit\TextUI\TestRunner') === false) {
+    // @codingStandardsIgnoreLine
     class_alias('PHPUnit_TextUI_TestRunner', 'PHPUnit'.'\TextUI\TestRunner');
 }
 
 if (class_exists('PHPUnit_Framework_TestResult') === true && class_exists('PHPUnit\Framework\TestResult') === false) {
+    // @codingStandardsIgnoreLine
     class_alias('PHPUnit_Framework_TestResult', 'PHPUnit'.'\Framework\TestResult');
 }
