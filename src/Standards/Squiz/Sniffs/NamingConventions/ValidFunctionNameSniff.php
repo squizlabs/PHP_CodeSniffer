ccacdd88f8a5d01f76302b8c333ef91d1a336fa4
jrfnl <jrfnl@users.noreply.github.com>
2018-02-10 04:54:37 +0100

    Squiz/ValidFunctionName: don't hide one message behind another

--- src/Standards/Squiz/Sniffs/NamingConventions/ValidFunctionNameSniff.php
+++ src/Standards/Squiz/Sniffs/NamingConventions/ValidFunctionNameSniff.php
@@ -39,7 +39,8 @@ class ValidFunctionNameSniff extends PEARValidFunctionNameSniff
         if (preg_match('|^__[^_]|', $functionName) !== 0) {
             $error = 'Function name "%s" is invalid; only PHP magic methods should be prefixed with a double underscore';
             $phpcsFile->addError($error, $stackPtr, 'DoubleUnderscore', $errorData);
-            return;
+
+            $functionName = ltrim($functionName, '_');
         }
 
         if (Common::isCamelCaps($functionName, false, true, false) === false) {
--- src/Standards/Squiz/Tests/NamingConventions/ValidFunctionNameUnitTest.php
+++ src/Standards/Squiz/Tests/NamingConventions/ValidFunctionNameUnitTest.php
@@ -35,7 +35,7 @@ class ValidFunctionNameUnitTest extends AbstractSniffUnitTest
             11 => 1,
             12 => 1,
             13 => 1,
-            14 => 1,
+            14 => 2,
         ];
 
     }//end getErrorList()
