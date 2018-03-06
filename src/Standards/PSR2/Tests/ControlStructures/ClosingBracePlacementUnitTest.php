e1d3a5b097887e8f6eabf1e229bdbe17f9ba7dd8
Greg Sherwood <gsherwood@squiz.net>
2018-03-06 14:49:04 +1100

    Added PSR2.ControlStructures.ClosingBracePlacement sniff to ensure control structure keywords are on the same line as the closing brace from the earlier body (ref #1857)

---
+++ src/Standards/PSR2/Tests/ControlStructures/ClosingBracePlacementUnitTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Unit test class for the ClosingBracePlacement sniff.
+ *
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006-2015 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ */
+
+namespace PHP_CodeSniffer\Standards\PSR2\Tests\ControlStructures;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+class ClosingBracePlacementUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return [
+            4  => 1,
+            6  => 1,
+            11 => 1,
+            13 => 1,
+            15 => 1,
+            17 => 1,
+            22 => 1,
+            31 => 1,
+            36 => 1,
+        ];
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+
+    }//end getWarningList()
+
+
+}//end class
