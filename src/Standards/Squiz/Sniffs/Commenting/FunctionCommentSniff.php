1dede2176a12f0f0aa93f602984651d8fa22e178
Greg Sherwood <gsherwood@squiz.net>
2018-03-12 11:04:37 +1100

    Squiz.Commenting.FunctionComment.IncorrectTypeHint message no longer contains cut-off suggested type hints

--- src/Standards/Squiz/Sniffs/Commenting/FunctionCommentSniff.php
+++ src/Standards/Squiz/Sniffs/Commenting/FunctionCommentSniff.php
@@ -423,8 +423,8 @@ class FunctionCommentSniff extends PEARFunctionCommentSniff
                 if ($suggestedTypeHint !== '' && isset($realParams[$pos]) === true) {
                     $typeHint = $realParams[$pos]['type_hint'];
 
-                    // Remove namespace prefixes.
-                    $suggestedTypeHint = substr($suggestedTypeHint, (strlen($typeHint) * -1));
+                    // Remove namespace prefixes when comparing.
+                    $compareTypeHint = substr($suggestedTypeHint, (strlen($typeHint) * -1));
 
                     if ($typeHint === '') {
                         $error = 'Type hint "%s" missing for %s';
@@ -443,7 +443,7 @@ class FunctionCommentSniff extends PEARFunctionCommentSniff
                         }
 
                         $phpcsFile->addError($error, $stackPtr, $errorCode, $data);
-                    } else if ($typeHint !== $suggestedTypeHint && $typeHint !== '?'.$suggestedTypeHint) {
+                    } else if ($typeHint !== $compareTypeHint && $typeHint !== '?'.$compareTypeHint) {
                         $error = 'Expected type hint "%s"; found "%s" for %s';
                         $data  = [
                             $suggestedTypeHint,
