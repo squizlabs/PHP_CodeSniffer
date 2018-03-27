9c3c1ed1730912543e455644fbf88f990bade4b9
Greg Sherwood <gsherwood@squiz.net>
2018-03-28 08:59:52 +1100

    Unit tests for #1971

--- src/Standards/Squiz/Tests/Arrays/ArrayDeclarationUnitTest.php
+++ src/Standards/Squiz/Tests/Arrays/ArrayDeclarationUnitTest.php
@@ -30,8 +30,9 @@ class ArrayDeclarationUnitTest extends AbstractSniffUnitTest
         switch ($testFile) {
         case 'ArrayDeclarationUnitTest.1.inc':
             return [
-                7   => 2,
-                9   => 2,
+                2   => 1,
+                8   => 2,
+                10  => 2,
                 22  => 1,
                 23  => 2,
                 24  => 2,
@@ -100,7 +101,8 @@ class ArrayDeclarationUnitTest extends AbstractSniffUnitTest
             ];
         case 'ArrayDeclarationUnitTest.2.inc':
             return [
-                9   => 1,
+                2 => 1,
+                10  => 1,
                 23  => 2,
                 24  => 2,
                 25  => 1,
