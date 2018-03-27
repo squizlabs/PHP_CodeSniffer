fe52812f049cb1d3247b328e4891b97fac069ba9
Greg Sherwood <gsherwood@squiz.net>
2018-03-28 08:46:51 +1100

    Fixed bug #1971 : Short array list syntax not correctly tokenized if short array is the first content in a file

--- src/Tokenizers/PHP.php
+++ src/Tokenizers/PHP.php
@@ -1549,7 +1549,7 @@ class PHP extends Tokenizer
                     T_CONSTANT_ENCAPSED_STRING => T_CONSTANT_ENCAPSED_STRING,
                 ];
 
-                for ($x = ($i - 1); $x > 0; $x--) {
+                for ($x = ($i - 1); $x >= 0; $x--) {
                     // If we hit a scope opener, the statement has ended
                     // without finding anything, so it's probably an array
                     // using PHP 7.1 short list syntax.
