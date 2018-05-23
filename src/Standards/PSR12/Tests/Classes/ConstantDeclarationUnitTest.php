5cb88c09300a2896770862d9d1ee0a034c4aeca3
jrfnl <jrfnl@users.noreply.github.com>
2018-05-23 14:17:31 +0200

    ConstantDeclaration: simplify the unit tests
    The sniff unit tests should test the functioning of the sniff which can be done perfectly well using the PHP version as used to run the sniff.
    
    Looks like what you are/were trying to do now was basically testing whether the `Config` class works as expected from within a sniff and while I agree, it may be a good idea to add unit tests for the `Config` class, this is not the task of a sniff to do.
    
    This PR simplifies the unit test code to defer to the PHP version used to run the sniff to test it.

--- src/Standards/PSR12/Tests/Classes/ConstantDeclarationUnitTest.php
+++ src/Standards/PSR12/Tests/Classes/ConstantDeclarationUnitTest.php
@@ -13,32 +13,6 @@ use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
 
 class ConstantDeclarationUnitTest extends AbstractSniffUnitTest
 {
-    const FILENAME_PHP_VERSION_70000 = 'ConstantDeclarationUnitTest.1.inc';
-
-    const FILENAME_PHP_VERSION_70100 = 'ConstantDeclarationUnitTest.2.inc';
-
-
-    /**
-     * Get a list of CLI values to set before the file is tested.
-     *
-     * @param string                  $filename The name of the file being tested.
-     * @param \PHP_CodeSniffer\Config $config   The config data for the run.
-     *
-     * @return void
-     */
-    public function setCliValues($filename, $config)
-    {
-        if ($filename === self::FILENAME_PHP_VERSION_70000) {
-            $config->setCommandLineValues(['--runtime-set', 'php_version', '70001']);
-        }
-
-        if ($filename === self::FILENAME_PHP_VERSION_70100) {
-            $config->setCommandLineValues(['--runtime-set', 'php_version', '70101']);
-        }
-
-        parent::setCliValues($filename, $config);
-
-    }//end setCliValues()
 
 
     /**
@@ -47,14 +21,12 @@ class ConstantDeclarationUnitTest extends AbstractSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of errors that should occur on that line.
      *
-     * @param string $filename The name of the file being tested.
-     *
      * @return array<int, int>
      */
-    protected function getErrorList($filename='')
+    protected function getErrorList()
     {
-        if ($filename === self::FILENAME_PHP_VERSION_70100) {
-            return [12 => 1];
+        if (PHP_VERSION_ID >= 70100) {
+            return [11 => 1];
         }
 
         return [];
