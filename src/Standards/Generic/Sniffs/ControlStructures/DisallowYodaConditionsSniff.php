55785b0928429485ef03ad30a4599bedf0bdbd3c
George Mponos <gmponos@gmail.com>
2018-11-13 23:05:17 +0200

    Disallow yoda conditions

--- src/Standards/Generic/Sniffs/ControlStructures/DisallowYodaConditionsSniff.php
+++ src/Standards/Generic/Sniffs/ControlStructures/DisallowYodaConditionsSniff.php
@@ -48,6 +48,7 @@ class DisallowYodaConditionsSniff implements Sniff
         $tokens    = $phpcsFile->getTokens();
         $nextIndex = $phpcsFile->findNext(Tokens::$emptyTokens, ($stackPtr + 1), null, true);
 
+        // Check the token after the comparison. If it is one of these then it is not a Yoda condition.
         if ($nextIndex === false || in_array(
             $tokens[$nextIndex]['code'],
             [
@@ -67,22 +68,51 @@ class DisallowYodaConditionsSniff implements Sniff
         }
 
         $previousIndex = $phpcsFile->findPrevious(Tokens::$emptyTokens, ($stackPtr - 1), null, true);
-        if ($previousIndex === false || in_array(
-            $tokens[$previousIndex]['code'],
-            [
-                T_VARIABLE,
-                T_STRING,
-            ],
-            true
-        ) === true
-        ) {
+
+        if ($previousIndex === false || in_array($tokens[$previousIndex]['code'], [
+                T_CLOSE_SHORT_ARRAY,
+                T_CLOSE_PARENTHESIS,
+                T_TRUE,
+                T_FALSE,
+                T_NULL,
+                T_LNUMBER,
+                T_DNUMBER,
+                T_CONSTANT_ENCAPSED_STRING,
+            ], true) === false) {
             return;
         }
 
+        if ($tokens[$previousIndex]['code'] === T_CLOSE_SHORT_ARRAY) {
+            $previousIndex = $tokens[$previousIndex]['bracket_opener'];
+        }
+        $prevIndex = $phpcsFile->findPrevious(Tokens::$emptyTokens, $previousIndex - 1, null, true);
+        if ($prevIndex === false) {
+            return;
+        }
+        if (in_array($tokens[$prevIndex]['code'], Tokens::$arithmeticTokens, true)) {
+            return;
+        }
+        if ($tokens[$prevIndex]['code'] === T_STRING_CONCAT) {
+            return;
+        }
+
+        // Is it a parenthesis.
         if ($tokens[$previousIndex]['code'] === T_CLOSE_PARENTHESIS) {
-            $found = $phpcsFile->findPrevious([T_VARIABLE], ($previousIndex - 1), $tokens[$previousIndex]['parenthesis_opener']);
-            if ($found !== false) {
-                return;
+            // Check what exists inside the parenthesis.
+            $closeParenthesisIndex = $phpcsFile->findPrevious(Tokens::$emptyTokens, ($tokens[$previousIndex]['parenthesis_opener'] - 1), null, true);
+
+            if ($closeParenthesisIndex === false || $tokens[$closeParenthesisIndex]['code'] !== T_ARRAY) {
+                if ($tokens[$closeParenthesisIndex]['code'] === T_STRING) {
+                    return;
+                }
+
+                // If it is not an array check what is inside.
+                $found = $phpcsFile->findPrevious([T_VARIABLE], ($previousIndex - 1), $tokens[$previousIndex]['parenthesis_opener']);
+
+                // If a variable exists it is not Yoda.
+                if ($found !== false) {
+                    return;
+                }
             }
         }
 
