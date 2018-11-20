c34146f607c19206453a9cfc2b133f373f232045
jrfnl <jrfnl@users.noreply.github.com>
2018-11-20 01:15:33 +0100

    Generic/SpaceAfterNot: make the sniff configurable
    This PR makes the `Generic.Formatting.SpaceAfterNot` sniff configurable as discussed in PR 2057.
    
    Notes:
    * Adds a public `spacing` property. Defaults to `1` to maintain BC.
    * Adds a public `ignoreNewLines` property. Defaults to `false` to maintain BC.
    * Adds a new non-fixable `CommentFound` error for when non-whitespace tokens are found between the NOT operator and the next non-empty token.
    * Adjusts the error message for the `Incorrect` error to allow for the flexibility now needed, what with the configurable `spacing` property.
        Note: the error **code** has not been changed to prevent a BC-break.
    * Makes the fixer more efficient compared to before.
        Previously, if a whitespace + new line + whitespace would be found with `spacing` set to `1`, the fixer would need three loops to apply all the fixes, now this is fixed in one go.
    
    Includes extensive additional unit tests.
    Unit tests have been added to the PHP file, but the changes should work just as well for JS.
    
    Includes basic xml documentation for the sniff.

--- src/Standards/Generic/Sniffs/Formatting/SpaceAfterNotSniff.php
+++ src/Standards/Generic/Sniffs/Formatting/SpaceAfterNotSniff.php
@@ -11,6 +11,7 @@ namespace PHP_CodeSniffer\Standards\Generic\Sniffs\Formatting;
 
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Util\Tokens;
 
 class SpaceAfterNotSniff implements Sniff
 {
@@ -24,6 +25,20 @@ class SpaceAfterNotSniff implements Sniff
         'PHP',
         'JS',
     ];
+
+    /**
+     * The number of spaces desired after the NOT operator.
+     *
+     * @var integer
+     */
+    public $spacing = 1;
+
+    /**
+     * Allow newlines instead of spaces.
+     *
+     * @var boolean
+     */
+    public $ignoreNewlines = false;
 
 
     /**
@@ -49,25 +64,78 @@ class SpaceAfterNotSniff implements Sniff
      */
     public function process(File $phpcsFile, $stackPtr)
     {
-        $tokens = $phpcsFile->getTokens();
+        $tokens        = $phpcsFile->getTokens();
+        $this->spacing = (int) $this->spacing;
 
-        $spacing = 0;
-        if ($tokens[($stackPtr + 1)]['code'] === T_WHITESPACE) {
-            $spacing = $tokens[($stackPtr + 1)]['length'];
-        }
-
-        if ($spacing === 1) {
+        $nextNonEmpty = $phpcsFile->findNext(Tokens::$emptyTokens, ($stackPtr + 1), null, true);
+        if ($nextNonEmpty === false) {
             return;
         }
 
-        $message = 'There must be a single space after a NOT operator; %s found';
-        $fix     = $phpcsFile->addFixableError($message, $stackPtr, 'Incorrect', [$spacing]);
+        if ($this->ignoreNewlines === true
+            && $tokens[$stackPtr]['line'] !== $tokens[$nextNonEmpty]['line']
+        ) {
+            return;
+        }
+
+        if ($this->spacing === 0 && $nextNonEmpty === ($stackPtr + 1)) {
+            return;
+        }
+
+        $maybePlural = '';
+        if ($this->spacing !== 1) {
+            $maybePlural = 's';
+        }
+
+        $nextNonWhitespace = $phpcsFile->findNext(T_WHITESPACE, ($stackPtr + 1), null, true);
+        if ($nextNonEmpty !== $nextNonWhitespace) {
+            $error = 'Expected %s space%s after NOT operator; comment found';
+            $data  = [
+                $this->spacing,
+                $maybePlural,
+            ];
+            $phpcsFile->addError($error, $stackPtr, 'CommentFound', $data);
+
+            return;
+        }
+
+        $found = 0;
+        if ($tokens[$stackPtr]['line'] !== $tokens[$nextNonEmpty]['line']) {
+            $found = 'newline';
+        } else if ($tokens[($stackPtr + 1)]['code'] === T_WHITESPACE) {
+            $found = $tokens[($stackPtr + 1)]['length'];
+        }
+
+        if ($found === $this->spacing) {
+            return;
+        }
+
+        $error = 'Expected %s space%s after NOT operator; %s found';
+        $data  = [
+            $this->spacing,
+            $maybePlural,
+            $found,
+        ];
+        $fix   = $phpcsFile->addFixableError($error, $stackPtr, 'Incorrect', $data);
 
         if ($fix === true) {
-            if ($spacing === 0) {
-                $phpcsFile->fixer->addContent($stackPtr, ' ');
+            $padding = str_repeat(' ', $this->spacing);
+            if ($found === 0) {
+                $phpcsFile->fixer->addContent($stackPtr, $padding);
             } else {
-                $phpcsFile->fixer->replaceToken(($stackPtr + 1), ' ');
+                $phpcsFile->fixer->beginChangeset();
+                $start = ($stackPtr + 1);
+
+                if ($this->spacing > 0) {
+                    $phpcsFile->fixer->replaceToken($start, $padding);
+                    ++$start;
+                }
+
+                for ($i = $start; $i < $nextNonWhitespace; $i++) {
+                    $phpcsFile->fixer->replaceToken($i, '');
+                }
+
+                $phpcsFile->fixer->endChangeset();
             }
         }
 
