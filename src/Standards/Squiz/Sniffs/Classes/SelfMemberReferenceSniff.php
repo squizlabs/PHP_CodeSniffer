e5885368dc034462a50231bc119fbad8aebe84d5
jrfnl <jrfnl@users.noreply.github.com>
2018-11-21 07:44:57 +0100

    Squiz/SelfMemberReference: allow for scoped namespace declarations

--- src/Standards/Squiz/Sniffs/Classes/SelfMemberReferenceSniff.php
+++ src/Standards/Squiz/Sniffs/Classes/SelfMemberReferenceSniff.php
@@ -208,7 +208,7 @@ class SelfMemberReferenceSniff extends AbstractScopeSniff
         $namespaceDeclaration = $phpcsFile->findPrevious(T_NAMESPACE, $stackPtr);
 
         if ($namespaceDeclaration !== false) {
-            $endOfNamespaceDeclaration = $phpcsFile->findNext(T_SEMICOLON, $namespaceDeclaration);
+            $endOfNamespaceDeclaration = $phpcsFile->findNext([T_SEMICOLON, T_OPEN_CURLY_BRACKET], $namespaceDeclaration);
             $namespace = $this->getDeclarationNameWithNamespace(
                 $phpcsFile->getTokens(),
                 ($endOfNamespaceDeclaration - 1)
