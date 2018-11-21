4cde8d9d935d16685d2db874260402b9fa6ae637
Greg Sherwood <gsherwood@squiz.net>
2018-11-21 11:27:54 +1100

    Fixed bug #2236 : Memory info masurement unit is Mb but probably should be MB

--- src/Util/Timing.php
+++ src/Util/Timing.php
@@ -75,7 +75,7 @@ class Timing
             $time = round($time).'ms';
         }
 
-        $mem = round((memory_get_peak_usage(true) / (1024 * 1024)), 2).'Mb';
+        $mem = round((memory_get_peak_usage(true) / (1024 * 1024)), 2).'MB';
         echo "Time: $time; Memory: $mem".PHP_EOL.PHP_EOL;
 
         self::$printed = true;
