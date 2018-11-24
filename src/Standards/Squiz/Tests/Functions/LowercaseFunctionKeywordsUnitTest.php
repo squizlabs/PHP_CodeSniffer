f5f6d42777bdcf53e524852038b16886712ed974
jrfnl <jrfnl@users.noreply.github.com>
2018-11-24 21:03:45 +0100

    Squiz/LowercaseFunctionKeywords: various improvements
    * Also check closures and the `abstract` and `final` method prefixes.
    * Make the error fixable.
    * Minor code duplication removal.
    
    Includes improved unit tests + added a `fixed` file.

--- src/Standards/Squiz/Tests/Functions/LowercaseFunctionKeywordsUnitTest.php
+++ src/Standards/Squiz/Tests/Functions/LowercaseFunctionKeywordsUnitTest.php
@@ -26,11 +26,14 @@ class LowercaseFunctionKeywordsUnitTest extends AbstractSniffUnitTest
     public function getErrorList()
     {
         return [
-            2 => 1,
-            3 => 1,
-            4 => 1,
-            5 => 1,
-            6 => 1,
+            16 => 1,
+            17 => 1,
+            20 => 1,
+            21 => 1,
+            22 => 1,
+            23 => 1,
+            24 => 3,
+            25 => 4,
         ];
 
     }//end getErrorList()
