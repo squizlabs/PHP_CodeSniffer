5cbef5e2ebf887259862062a6b81eceb77c08a29
Marius <marius@cart.lt>
2019-01-17 12:47:01 +0200

    Fix AjaxNullComparisonSniff to ignore files with no doc comments

--- src/Standards/MySource/Sniffs/PHP/AjaxNullComparisonSniff.php
+++ src/Standards/MySource/Sniffs/PHP/AjaxNullComparisonSniff.php
@@ -47,7 +47,12 @@ class AjaxNullComparisonSniff implements Sniff
         // Make sure it is an API function. We know this by the doc comment.
         $commentEnd   = $phpcsFile->findPrevious(T_DOC_COMMENT_CLOSE_TAG, $stackPtr);
         $commentStart = $phpcsFile->findPrevious(T_DOC_COMMENT_OPEN_TAG, ($commentEnd - 1));
-        $comment      = $phpcsFile->getTokensAsString($commentStart, ($commentEnd - $commentStart));
+        // If function doesn't contain any doc comments - skip it.
+        if ($commentEnd === false || $commentStart === false) {
+            return;
+        }
+
+        $comment = $phpcsFile->getTokensAsString($commentStart, ($commentEnd - $commentStart));
         if (strpos($comment, '* @api') === false) {
             return;
         }
