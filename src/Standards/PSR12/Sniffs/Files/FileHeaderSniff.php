a4ed059281e7e7380d452e757ec31d44aeb1867f
Craig Duncan <git@duncanc.co.uk>
2019-10-21 23:07:53 +0100

    Blocks should be separated rather than followed by a blank line

--- src/Standards/PSR12/Sniffs/Files/FileHeaderSniff.php
+++ src/Standards/PSR12/Sniffs/Files/FileHeaderSniff.php
@@ -173,7 +173,7 @@ class FileHeaderSniff implements Sniff
                 // this block.
                 $next = $phpcsFile->findNext(T_WHITESPACE, ($line['end'] + 1), null, true);
                 if ($next !== false && $tokens[$next]['line'] !== ($tokens[$line['end']]['line'] + 2)) {
-                    $error = 'Header blocks must be followed by a single blank line';
+                    $error = 'Header blocks must be separated by a single blank line';
                     $fix   = $phpcsFile->addFixableError($error, $line['end'], 'SpacingAfterBlock');
                     if ($fix === true) {
                         if ($tokens[$next]['line'] === $tokens[$line['end']]['line']) {
