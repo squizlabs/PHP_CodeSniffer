282306f29fe39a829db577460db261b8d69dd957
Greg Sherwood <gsherwood@squiz.net>
2019-11-20 08:41:57 +1100

    Fixed alignment false positive when using arrow function as a array value (ref #2703, #2523)

--- src/Standards/Squiz/Sniffs/Arrays/ArrayDeclarationSniff.php
+++ src/Standards/Squiz/Sniffs/Arrays/ArrayDeclarationSniff.php
@@ -367,6 +367,7 @@ class ArrayDeclarationSniff implements Sniff
             if ($tokens[$nextToken]['code'] === T_ARRAY
                 || $tokens[$nextToken]['code'] === T_OPEN_SHORT_ARRAY
                 || $tokens[$nextToken]['code'] === T_CLOSURE
+                || $tokens[$nextToken]['code'] === T_FN
             ) {
                 // Let subsequent calls of this test handle nested arrays.
                 if ($tokens[$lastToken]['code'] !== T_DOUBLE_ARROW) {
