30cee01494aaafa0718d6d478e3461ddda6f9f7d
Vincent Langlet <vincentlanglet@hotmail.fr>
2019-11-30 11:38:37 +0100

    Add fixer

--- src/Standards/Squiz/Sniffs/Operators/ValidLogicalOperatorsSniff.php
+++ src/Standards/Squiz/Sniffs/Operators/ValidLogicalOperatorsSniff.php
@@ -59,7 +59,11 @@ class ValidLogicalOperatorsSniff implements Sniff
             $operator,
             $replacements[$operator],
         ];
-        $phpcsFile->addError($error, $stackPtr, 'NotAllowed', $data);
+        $fix   = $phpcsFile->addFixableError($error, $stackPtr, 'NotAllowed', $data);
+
+        if ($fix === true) {
+            $phpcsFile->fixer->replaceToken($stackPtr, $replacements[$operator]);
+        }
 
     }//end process()
 
