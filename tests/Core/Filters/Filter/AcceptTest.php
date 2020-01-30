91fa3b6756c162f42ed258b85940f88db0319257
Greg Sherwood <gsherwood@squiz.net>
2020-01-31 09:09:57 +1100

    Skip tests when running in a PEAR install due to file path differences

--- tests/Core/Filters/Filter/AcceptTest.php
+++ tests/Core/Filters/Filter/AcceptTest.php
@@ -34,12 +34,34 @@ class AcceptTest extends TestCase
 
 
     /**
+     * Initialize the test.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        if ($GLOBALS['PHP_CODESNIFFER_PEAR'] === true) {
+            // PEAR installs test and sniff files into different locations
+            // so these tests will not pass as they directly reference files
+            // by relative location.
+            $this->markTestSkipped('Test cannot run from a PEAR install');
+        }
+
+    }//end setUp()
+
+
+    /**
      * Initialize the config and ruleset objects based on the `AcceptTest.xml` ruleset file.
      *
      * @return void
      */
     public static function setUpBeforeClass()
     {
+        if ($GLOBALS['PHP_CODESNIFFER_PEAR'] === true) {
+            // This test will be skipped.
+            return;
+        }
+
         $standard      = __DIR__.'/'.basename(__FILE__, '.php').'.xml';
         self::$config  = new Config(["--standard=$standard", "--ignore=*/somethingelse/*"]);
         self::$ruleset = new Ruleset(self::$config);
--- tests/Core/Ruleset/RuleInclusionTest.php
+++ tests/Core/Ruleset/RuleInclusionTest.php
@@ -25,12 +25,34 @@ class RuleInclusionTest extends TestCase
 
 
     /**
+     * Initialize the test.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        if ($GLOBALS['PHP_CODESNIFFER_PEAR'] === true) {
+            // PEAR installs test and sniff files into different locations
+            // so these tests will not pass as they directly reference files
+            // by relative location.
+            $this->markTestSkipped('Test cannot run from a PEAR install');
+        }
+
+    }//end setUp()
+
+
+    /**
      * Initialize the config and ruleset objects based on the `RuleInclusionTest.xml` ruleset file.
      *
      * @return void
      */
     public static function setUpBeforeClass()
     {
+        if ($GLOBALS['PHP_CODESNIFFER_PEAR'] === true) {
+            // This test will be skipped.
+            return;
+        }
+
         $standard      = __DIR__.'/'.basename(__FILE__, '.php').'.xml';
         $config        = new Config(["--standard=$standard"]);
         self::$ruleset = new Ruleset($config);
