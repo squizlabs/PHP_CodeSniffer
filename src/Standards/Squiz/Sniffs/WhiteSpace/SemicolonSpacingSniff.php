054762663a9204f8a703cfd5acd3a32e72b74e41
Greg Sherwood <gsherwood@squiz.net>
2020-02-19 09:34:35 +1100

    Fixed bug #2810 : PHPCBF fails to fix file with empty statement at start on control structure

--- src/Standards/Squiz/Sniffs/WhiteSpace/SemicolonSpacingSniff.php
+++ src/Standards/Squiz/Sniffs/WhiteSpace/SemicolonSpacingSniff.php
@@ -59,7 +59,7 @@ class SemicolonSpacingSniff implements Sniff
 
         $nonSpace = $phpcsFile->findPrevious(Tokens::$emptyTokens, ($stackPtr - 2), null, true);
 
-        // Detect whether this is a semi-colons for a conditions in a `for()` control structure.
+        // Detect whether this is a semi-colon for a condition in a `for()` control structure.
         $forCondition = false;
         if (isset($tokens[$stackPtr]['nested_parenthesis']) === true) {
             $nestedParens     = $tokens[$stackPtr]['nested_parenthesis'];
@@ -77,6 +77,7 @@ class SemicolonSpacingSniff implements Sniff
 
         if ($tokens[$nonSpace]['code'] === T_SEMICOLON
             || ($forCondition === true && $nonSpace === $tokens[$owner]['parenthesis_opener'])
+            || (isset($tokens[$nonSpace]['scope_opener']) === true)
         ) {
             // Empty statement.
             return;
