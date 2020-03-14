c35bd8ce5abe516850931f7c205d883694dbc91d
jrfnl <jrfnl@users.noreply.github.com>
2020-03-14 04:16:14 +0100

    PHPCS 4.0: update the PHPUnit requirements
    Per commit 25324b37c1a947b1a70e840d67d156542d53c50b, the minimum PHP version for PHPCS 4.x will be PHP 7.0. This means that PHPUnit 4.x and 5.x [no longer need to be supported](https://phpunit.de/supported-versions.html).

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,23 +27,6 @@ if (is_file(__DIR__.'/../autoload.php') === true) {
 
 $tokens = new \PHP_CodeSniffer\Util\Tokens();
 
-// Compatibility for PHPUnit < 6 and PHPUnit 6+.
-if (class_exists('PHPUnit_Framework_TestSuite') === true && class_exists('PHPUnit\Framework\TestSuite') === false) {
-    class_alias('PHPUnit_Framework_TestSuite', 'PHPUnit'.'\Framework\TestSuite');
-}
-
-if (class_exists('PHPUnit_Framework_TestCase') === true && class_exists('PHPUnit\Framework\TestCase') === false) {
-    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit'.'\Framework\TestCase');
-}
-
-if (class_exists('PHPUnit_TextUI_TestRunner') === true && class_exists('PHPUnit\TextUI\TestRunner') === false) {
-    class_alias('PHPUnit_TextUI_TestRunner', 'PHPUnit'.'\TextUI\TestRunner');
-}
-
-if (class_exists('PHPUnit_Framework_TestResult') === true && class_exists('PHPUnit\Framework\TestResult') === false) {
-    class_alias('PHPUnit_Framework_TestResult', 'PHPUnit'.'\Framework\TestResult');
-}
-
 
 /**
  * A global util function to help print unit test fixing data.
