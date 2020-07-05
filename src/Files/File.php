38c01b340a204b2f6b3f635436f4e1168349512c
jrfnl <jrfnl@users.noreply.github.com>
2020-07-05 07:32:55 +0200

    PHP 8.0 | File::getMethodProperties(): add support for "union" return types
    This adds support for union types to the `File::getMethodProperties()` method.
    
    Includes extensive unit tests.

--- src/Files/File.php
+++ src/Files/File.php
@@ -1634,8 +1634,11 @@ class File
                 T_SELF         => T_SELF,
                 T_PARENT       => T_PARENT,
                 T_STATIC       => T_STATIC,
+                T_FALSE        => T_FALSE,
+                T_NULL         => T_NULL,
                 T_NAMESPACE    => T_NAMESPACE,
                 T_NS_SEPARATOR => T_NS_SEPARATOR,
+                T_TYPE_UNION   => T_TYPE_UNION,
             ];
 
             for ($i = $this->tokens[$stackPtr]['parenthesis_closer']; $i < $this->numTokens; $i++) {
