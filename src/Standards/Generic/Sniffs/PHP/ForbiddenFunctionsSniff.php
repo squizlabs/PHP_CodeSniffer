bf708e65cabf9d3b30e1542da58d11641a09f872
jrfnl <jrfnl@users.noreply.github.com>
2020-08-01 01:48:56 +0200

    PHP 8.0 | Generic/ForbiddenFunctions: allow for nullsafe object operator
    Includes unit test.

--- src/Standards/Generic/Sniffs/PHP/ForbiddenFunctionsSniff.php
+++ src/Standards/Generic/Sniffs/PHP/ForbiddenFunctionsSniff.php
@@ -120,18 +120,19 @@ class ForbiddenFunctionsSniff implements Sniff
         $tokens = $phpcsFile->getTokens();
 
         $ignore = [
-            T_DOUBLE_COLON    => true,
-            T_OBJECT_OPERATOR => true,
-            T_FUNCTION        => true,
-            T_CONST           => true,
-            T_PUBLIC          => true,
-            T_PRIVATE         => true,
-            T_PROTECTED       => true,
-            T_AS              => true,
-            T_NEW             => true,
-            T_INSTEADOF       => true,
-            T_NS_SEPARATOR    => true,
-            T_IMPLEMENTS      => true,
+            T_DOUBLE_COLON             => true,
+            T_OBJECT_OPERATOR          => true,
+            T_NULLSAFE_OBJECT_OPERATOR => true,
+            T_FUNCTION                 => true,
+            T_CONST                    => true,
+            T_PUBLIC                   => true,
+            T_PRIVATE                  => true,
+            T_PROTECTED                => true,
+            T_AS                       => true,
+            T_NEW                      => true,
+            T_INSTEADOF                => true,
+            T_NS_SEPARATOR             => true,
+            T_IMPLEMENTS               => true,
         ];
 
         $prevToken = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
