460f212c4539dc93de9980847dfcb0735c9be110
jrfnl <jrfnl@users.noreply.github.com>
2020-08-01 01:50:44 +0200

    PHP 8.0 | PEAR/ObjectOperatorIndent: sniff for nullsafe object operator
    This adds sniffing for the indentation of nullsafe object operators to the sniff.
    
    Includes unit test.

--- src/Standards/PEAR/Sniffs/WhiteSpace/ObjectOperatorIndentSniff.php
+++ src/Standards/PEAR/Sniffs/WhiteSpace/ObjectOperatorIndentSniff.php
@@ -29,6 +29,16 @@ class ObjectOperatorIndentSniff implements Sniff
      */
     public $multilevel = false;
 
+    /**
+     * Tokens to listen for.
+     *
+     * @var array
+     */
+    private $targets = [
+        T_OBJECT_OPERATOR,
+        T_NULLSAFE_OBJECT_OPERATOR,
+    ];
+
 
     /**
      * Returns an array of tokens this test wants to listen for.
@@ -37,7 +47,7 @@ class ObjectOperatorIndentSniff implements Sniff
      */
     public function register()
     {
-        return [T_OBJECT_OPERATOR];
+        return $this->targets;
 
     }//end register()
 
@@ -57,14 +67,14 @@ class ObjectOperatorIndentSniff implements Sniff
 
         // Make sure this is the first object operator in a chain of them.
         $start = $phpcsFile->findStartOfStatement($stackPtr);
-        $prev  = $phpcsFile->findPrevious(T_OBJECT_OPERATOR, ($stackPtr - 1), $start);
+        $prev  = $phpcsFile->findPrevious($this->targets, ($stackPtr - 1), $start);
         if ($prev !== false) {
             return;
         }
 
         // Make sure this is a chained call.
         $end  = $phpcsFile->findEndOfStatement($stackPtr);
-        $next = $phpcsFile->findNext(T_OBJECT_OPERATOR, ($stackPtr + 1), $end);
+        $next = $phpcsFile->findNext($this->targets, ($stackPtr + 1), $end);
         if ($next === false) {
             // Not a chained call.
             return;
@@ -179,7 +189,7 @@ class ObjectOperatorIndentSniff implements Sniff
             }//end if
 
             $next = $phpcsFile->findNext(
-                T_OBJECT_OPERATOR,
+                $this->targets,
                 ($next + 1),
                 null,
                 false,
