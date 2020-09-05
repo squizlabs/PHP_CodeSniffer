dedda1ee36bac5cdbe766498177d715ab6e34c2a
Sergei Morozov <morozov@tut.by>
2020-09-04 19:56:57 -0700

    Ignore the previous element's T_COMMA when looking up the beginning of the previous expression

--- src/Standards/Generic/Sniffs/Arrays/ArrayIndentSniff.php
+++ src/Standards/Generic/Sniffs/Arrays/ArrayIndentSniff.php
@@ -62,6 +62,7 @@ class ArrayIndentSniff extends AbstractArraySniff
         // Determine how far indented the entire array declaration should be.
         $ignore     = Tokens::$emptyTokens;
         $ignore[]   = T_DOUBLE_ARROW;
+        $ignore[]   = T_COMMA;
         $prev       = $phpcsFile->findPrevious($ignore, ($stackPtr - 1), null, true);
         $start      = $phpcsFile->findStartOfStatement($prev);
         $first      = $phpcsFile->findFirstOnLine(T_WHITESPACE, $start, true);
