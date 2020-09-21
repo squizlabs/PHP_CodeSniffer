e94627c06d1f005f92cf2abce28366827c92665e
Greg Sherwood <gsherwood@squiz.net>
2020-09-21 14:55:28 +1000

    Moved tests around to group them (ref #3101)

--- src/Standards/Generic/Tests/Arrays/ArrayIndentUnitTest.php
+++ src/Standards/Generic/Tests/Arrays/ArrayIndentUnitTest.php
@@ -36,13 +36,13 @@ class ArrayIndentUnitTest extends AbstractSniffUnitTest
             62 => 1,
             63 => 1,
             69 => 1,
-            76 => 1,
             77 => 1,
             78 => 1,
             79 => 1,
+            85 => 1,
+            86 => 1,
             87 => 1,
             88 => 1,
-            89 => 1,
         ];
 
     }//end getErrorList()
