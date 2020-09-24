4377de2e1405fceeb7954195d2c19e11ea595dc2
Greg Sherwood <gsherwood@squiz.net>
2020-09-24 14:28:23 +1000

    Fixed bug #3053 : PSR2 incorrect fix when multiple use statements on same line do not have whitespace between them

--- src/Standards/PSR2/Tests/Namespaces/UseDeclarationUnitTest.php
+++ src/Standards/PSR2/Tests/Namespaces/UseDeclarationUnitTest.php
@@ -34,7 +34,10 @@ class UseDeclarationUnitTest extends AbstractSniffUnitTest
                 5  => 1,
                 6  => 1,
                 7  => 1,
-                12 => 1,
+                9  => 1,
+                10 => 1,
+                11 => 1,
+                16 => 1,
             ];
         case 'UseDeclarationUnitTest.3.inc':
             return [
