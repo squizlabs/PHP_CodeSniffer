4cb3d16bd614e4d8a244329dfb4affbae5423b40
jrfnl <jrfnl@users.noreply.github.com>
2021-03-04 05:34:52 +0100

    PHP 8.0 | Generic/NoSilencedErrors: add tests with named function call parameters
    ... to confirm that the code snippet in the error message will be correct (set the `$contextLength` to a higher value to confirm).

--- src/Standards/Generic/Sniffs/PHP/NoSilencedErrorsSniff.php
+++ src/Standards/Generic/Sniffs/PHP/NoSilencedErrorsSniff.php
@@ -55,7 +55,7 @@ class NoSilencedErrorsSniff implements Sniff
     {
         // Prepare the "Found" string to display.
         $contextLength  = 4;
-        $endOfStatement = $phpcsFile->findEndOfStatement($stackPtr, T_COMMA);
+        $endOfStatement = $phpcsFile->findEndOfStatement($stackPtr, [T_COMMA, T_COLON]);
         if (($endOfStatement - $stackPtr) < $contextLength) {
             $contextLength = ($endOfStatement - $stackPtr);
         }
