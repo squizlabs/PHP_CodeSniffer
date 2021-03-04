236fffc17a19ac69de42caa0863674f951bafc3b
jrfnl <jrfnl@users.noreply.github.com>
2021-03-04 04:41:41 +0100

    PHP 8.0 | Pear/FunctionCallSignature: support named parameters
    The `File::findEndOfStatement()` method regards a `T_COLON` as the end of a statement, while for function call arguments using named parameters, the colon is part of the parameter name declaration and should be disregarded when determining the end of the statement.
    
    Fixed now.
    
    Includes unit tests.

--- src/Standards/PEAR/Sniffs/Functions/FunctionCallSignatureSniff.php
+++ src/Standards/PEAR/Sniffs/Functions/FunctionCallSignatureSniff.php
@@ -581,7 +581,7 @@ class FunctionCallSignatureSniff implements Sniff
 
                 if ($inArg === false) {
                     $argStart = $nextCode;
-                    $argEnd   = $phpcsFile->findEndOfStatement($nextCode);
+                    $argEnd   = $phpcsFile->findEndOfStatement($nextCode, [T_COLON]);
                 }
             }//end if
 
@@ -618,7 +618,7 @@ class FunctionCallSignatureSniff implements Sniff
                 }//end if
 
                 $argStart = $next;
-                $argEnd   = $phpcsFile->findEndOfStatement($next);
+                $argEnd   = $phpcsFile->findEndOfStatement($next, [T_COLON]);
             }//end if
         }//end for
 
