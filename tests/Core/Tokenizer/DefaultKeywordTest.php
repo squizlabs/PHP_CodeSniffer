14ac7ebffc478a82a3a2e095c9b22bb3690430bc
jrfnl <jrfnl@users.noreply.github.com>
2021-03-25 11:50:13 +0100

    Tokenizer/PHP: add more tests for default keyword and double arrow
    In particular, this adds tests where the `default` keyword is not used as the keyword and should be tokenized as `T_STRING`.

--- tests/Core/Tokenizer/DefaultKeywordTest.php
+++ tests/Core/Tokenizer/DefaultKeywordTest.php
@@ -22,7 +22,8 @@ class DefaultKeywordTest extends AbstractMethodUnitTest
      * Note: Cases and default structures within a match structure do *NOT* get case/default scope
      * conditions, in contrast to case and default structures in switch control structures.
      *
-     * @param string $testMarker The comment prefacing the target token.
+     * @param string $testMarker  The comment prefacing the target token.
+     * @param string $testContent The token content to look for.
      *
      * @dataProvider dataMatchDefault
      * @covers       PHP_CodeSniffer\Tokenizers\PHP::tokenize
@@ -30,11 +31,11 @@ class DefaultKeywordTest extends AbstractMethodUnitTest
      *
      * @return void
      */
-    public function testMatchDefault($testMarker)
+    public function testMatchDefault($testMarker, $testContent='default')
     {
         $tokens = self::$phpcsFile->getTokens();
 
-        $token      = $this->getTargetToken($testMarker, [T_MATCH_DEFAULT, T_DEFAULT]);
+        $token      = $this->getTargetToken($testMarker, [T_MATCH_DEFAULT, T_DEFAULT, T_STRING], $testContent);
         $tokenArray = $tokens[$token];
 
         $this->assertSame(T_MATCH_DEFAULT, $tokenArray['code'], 'Token tokenized as '.$tokenArray['type'].', not T_MATCH_DEFAULT (code)');
@@ -57,11 +58,36 @@ class DefaultKeywordTest extends AbstractMethodUnitTest
     public function dataMatchDefault()
     {
         return [
-            'simple_match_default'            => ['/* testSimpleMatchDefault */'],
-            'match_default_in_switch_case_1'  => ['/* testMatchDefaultNestedInSwitchCase1 */'],
-            'match_default_in_switch_case_2'  => ['/* testMatchDefaultNestedInSwitchCase2 */'],
-            'match_default_in_switch_default' => ['/* testMatchDefaultNestedInSwitchDefault */'],
-            'match_default_containing_switch' => ['/* testMatchDefault */'],
+            'simple_match_default'                                    => ['/* testSimpleMatchDefault */'],
+            'match_default_in_switch_case_1'                          => ['/* testMatchDefaultNestedInSwitchCase1 */'],
+            'match_default_in_switch_case_2'                          => ['/* testMatchDefaultNestedInSwitchCase2 */'],
+            'match_default_in_switch_default'                         => ['/* testMatchDefaultNestedInSwitchDefault */'],
+            'match_default_containing_switch'                         => ['/* testMatchDefault */'],
+
+            'match_default_with_nested_long_array_and_default_key'    => [
+                '/* testMatchDefaultWithNestedLongArrayWithClassConstantKey */',
+                'DEFAULT',
+            ],
+            'match_default_with_nested_long_array_and_default_key_2'  => [
+                '/* testMatchDefaultWithNestedLongArrayWithClassConstantKeyLevelDown */',
+                'DEFAULT',
+            ],
+            'match_default_with_nested_short_array_and_default_key'   => [
+                '/* testMatchDefaultWithNestedShortArrayWithClassConstantKey */',
+                'DEFAULT',
+            ],
+            'match_default_with_nested_short_array_and_default_key_2' => [
+                '/* testMatchDefaultWithNestedShortArrayWithClassConstantKeyLevelDown */',
+                'DEFAULT',
+            ],
+            'match_default_in_long_array'                             => [
+                '/* testMatchDefaultNestedInLongArray */',
+                'DEFAULT',
+            ],
+            'match_default_in_short_array'                            => [
+                '/* testMatchDefaultNestedInShortArray */',
+                'DEFAULT',
+            ],
         ];
 
     }//end dataMatchDefault()
@@ -78,17 +104,18 @@ class DefaultKeywordTest extends AbstractMethodUnitTest
      * @param int      $openerOffset  The expected offset of the scope opener in relation to the testMarker.
      * @param int      $closerOffset  The expected offset of the scope closer in relation to the testMarker.
      * @param int|null $conditionStop The expected offset at which tokens stop having T_DEFAULT as a scope condition.
+     * @param string   $testContent   The token content to look for.
      *
      * @dataProvider dataSwitchDefault
      * @covers       PHP_CodeSniffer\Tokenizers\Tokenizer::recurseScopeMap
      *
      * @return void
      */
-    public function testSwitchDefault($testMarker, $openerOffset, $closerOffset, $conditionStop=null)
+    public function testSwitchDefault($testMarker, $openerOffset, $closerOffset, $conditionStop=null, $testContent='default')
     {
         $tokens = self::$phpcsFile->getTokens();
 
-        $token      = $this->getTargetToken($testMarker, [T_MATCH_DEFAULT, T_DEFAULT]);
+        $token      = $this->getTargetToken($testMarker, [T_MATCH_DEFAULT, T_DEFAULT, T_STRING], $testContent);
         $tokenArray = $tokens[$token];
         $expectedScopeOpener = ($token + $openerOffset);
         $expectedScopeCloser = ($token + $closerOffset);
@@ -180,6 +207,69 @@ class DefaultKeywordTest extends AbstractMethodUnitTest
         ];
 
     }//end dataSwitchDefault()
+
+
+    /**
+     * Verify that the retokenization of `T_DEFAULT` tokens in match constructs, doesn't negatively
+     * impact the tokenization of `T_STRING` tokens with the contents 'default' which aren't in
+     * actual fact the default keyword.
+     *
+     * @param string $testMarker  The comment prefacing the target token.
+     * @param string $testContent The token content to look for.
+     *
+     * @dataProvider dataNotDefaultKeyword
+     * @covers       PHP_CodeSniffer\Tokenizers\PHP::processAdditional
+     *
+     * @return void
+     */
+    public function testNotDefaultKeyword($testMarker, $testContent='DEFAULT')
+    {
+        $tokens = self::$phpcsFile->getTokens();
+
+        $token      = $this->getTargetToken($testMarker, [T_MATCH_DEFAULT, T_DEFAULT, T_STRING], $testContent);
+        $tokenArray = $tokens[$token];
+
+        $this->assertSame(T_STRING, $tokenArray['code'], 'Token tokenized as '.$tokenArray['type'].', not T_STRING (code)');
+        $this->assertSame('T_STRING', $tokenArray['type'], 'Token tokenized as '.$tokenArray['type'].', not T_STRING (type)');
+
+        $this->assertArrayNotHasKey('scope_condition', $tokenArray, 'Scope condition is set');
+        $this->assertArrayNotHasKey('scope_opener', $tokenArray, 'Scope opener is set');
+        $this->assertArrayNotHasKey('scope_closer', $tokenArray, 'Scope closer is set');
+
+    }//end testNotDefaultKeyword()
+
+
+    /**
+     * Data provider.
+     *
+     * @see testNotDefaultKeyword()
+     *
+     * @return array
+     */
+    public function dataNotDefaultKeyword()
+    {
+        return [
+            'class-constant-as-short-array-key'                   => ['/* testClassConstantAsShortArrayKey */'],
+            'class-property-as-short-array-key'                   => ['/* testClassPropertyAsShortArrayKey */'],
+            'namespaced-constant-as-short-array-key'              => ['/* testNamespacedConstantAsShortArrayKey */'],
+            'fqn-global-constant-as-short-array-key'              => ['/* testFQNGlobalConstantAsShortArrayKey */'],
+            'class-constant-as-long-array-key'                    => ['/* testClassConstantAsLongArrayKey */'],
+            'class-constant-as-yield-key'                         => ['/* testClassConstantAsYieldKey */'],
+
+            'class-constant-as-long-array-key-nested-in-match'    => ['/* testClassConstantAsLongArrayKeyNestedInMatch */'],
+            'class-constant-as-long-array-key-nested-in-match-2'  => ['/* testClassConstantAsLongArrayKeyNestedInMatchLevelDown */'],
+            'class-constant-as-short-array-key-nested-in-match'   => ['/* testClassConstantAsShortArrayKeyNestedInMatch */'],
+            'class-constant-as-short-array-key-nested-in-match-2' => ['/* testClassConstantAsShortArrayKeyNestedInMatchLevelDown */'],
+            'class-constant-as-long-array-key-with-nested-match'  => ['/* testClassConstantAsLongArrayKeyWithNestedMatch */'],
+            'class-constant-as-short-array-key-with-nested-match' => ['/* testClassConstantAsShortArrayKeyWithNestedMatch */'],
+
+            'class-constant-in-switch-case'                       => ['/* testClassConstantInSwitchCase */'],
+            'class-property-in-switch-case'                       => ['/* testClassPropertyInSwitchCase */'],
+            'namespaced-constant-in-switch-case'                  => ['/* testNamespacedConstantInSwitchCase */'],
+            'namespace-relative-constant-in-switch-case'          => ['/* testNamespaceRelativeConstantInSwitchCase */'],
+        ];
+
+    }//end dataNotDefaultKeyword()
 
 
 }//end class
