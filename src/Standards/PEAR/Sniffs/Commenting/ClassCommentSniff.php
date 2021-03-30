c132dc510b6f05c819404b2e43e2722409de605a
Greg Sherwood <gsherwood@squiz.net>
2021-03-31 10:14:33 +1100

    Added attribute support

--- src/Standards/PEAR/Sniffs/Commenting/ClassCommentSniff.php
+++ src/Standards/PEAR/Sniffs/Commenting/ClassCommentSniff.php
@@ -47,10 +47,24 @@ class ClassCommentSniff extends FileCommentSniff
         $type      = strtolower($tokens[$stackPtr]['content']);
         $errorData = [$type];
 
-        $find   = Tokens::$methodPrefixes;
-        $find[] = T_WHITESPACE;
+        $find = Tokens::$methodPrefixes;
+        $find[T_WHITESPACE] = T_WHITESPACE;
 
-        $commentEnd = $phpcsFile->findPrevious($find, ($stackPtr - 1), null, true);
+        for ($commentEnd = ($stackPtr - 1); $commentEnd >= 0; $commentEnd--) {
+            if (isset($find[$tokens[$commentEnd]['code']]) === true) {
+                continue;
+            }
+
+            if ($tokens[$commentEnd]['code'] === T_ATTRIBUTE_END
+                && isset($tokens[$commentEnd]['attribute_opener']) === true
+            ) {
+                $commentEnd = $tokens[$commentEnd]['attribute_opener'];
+                continue;
+            }
+
+            break;
+        }
+
         if ($tokens[$commentEnd]['code'] !== T_DOC_COMMENT_CLOSE_TAG
             && $tokens[$commentEnd]['code'] !== T_COMMENT
         ) {
