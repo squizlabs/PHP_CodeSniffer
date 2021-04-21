6568bd06380d98ce3e455bd6170c97cc8a434b76
jrfnl <jrfnl@users.noreply.github.com>
2021-04-22 01:13:09 +0200

    Generic/Syntax: check files using only short open echo tag
    Includes unit tests.

--- src/Standards/Generic/Tests/PHP/SyntaxUnitTest.php
+++ src/Standards/Generic/Tests/PHP/SyntaxUnitTest.php
@@ -22,11 +22,21 @@ class SyntaxUnitTest extends AbstractSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of errors that should occur on that line.
      *
+     * @param string $testFile The name of the file being tested.
+     *
      * @return array<int, int>
      */
-    public function getErrorList()
+    public function getErrorList($testFile='')
     {
-        return [3 => 1];
+        switch ($testFile) {
+        case 'SyntaxUnitTest.1.inc':
+        case 'SyntaxUnitTest.2.inc':
+            return [3 => 1];
+            break;
+        default:
+            return [];
+            break;
+        }
 
     }//end getErrorList()
 
