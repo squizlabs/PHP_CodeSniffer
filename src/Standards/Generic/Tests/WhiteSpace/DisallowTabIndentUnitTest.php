5df757d1ee240e0372a84e0450733ef977a3e231
jrfnl <jrfnl@users.noreply.github.com>
2021-04-22 03:52:38 +0200

    Generic/DisallowTabIndent: check files using only short open echo tag
    Includes unit tests.

--- src/Standards/Generic/Tests/WhiteSpace/DisallowTabIndentUnitTest.php
+++ src/Standards/Generic/Tests/WhiteSpace/DisallowTabIndentUnitTest.php
@@ -36,49 +36,68 @@ class DisallowTabIndentUnitTest extends AbstractSniffUnitTest
      * The key of the array should represent the line number and the value
      * should represent the number of errors that should occur on that line.
      *
+     * @param string $testFile The name of the file being tested.
+     *
      * @return array<int, int>
      */
-    public function getErrorList()
+    public function getErrorList($testFile='DisallowTabIndentUnitTest.1.inc')
     {
-        return [
-            5  => 2,
-            9  => 1,
-            15 => 1,
-            20 => 2,
-            21 => 1,
-            22 => 2,
-            23 => 1,
-            24 => 2,
-            31 => 1,
-            32 => 2,
-            33 => 2,
-            41 => 1,
-            42 => 1,
-            43 => 1,
-            44 => 1,
-            45 => 1,
-            46 => 1,
-            47 => 1,
-            48 => 1,
-            54 => 1,
-            55 => 1,
-            56 => 1,
-            57 => 1,
-            58 => 1,
-            59 => 1,
-            79 => 1,
-            80 => 1,
-            81 => 1,
-            82 => 1,
-            83 => 1,
-            85 => 1,
-            86 => 1,
-            87 => 1,
-            89 => 1,
-            90 => 1,
-            92 => 1,
-            93 => 1,
-        ];
+        switch ($testFile) {
+        case 'DisallowTabIndentUnitTest.1.inc':
+            return [
+                5  => 2,
+                9  => 1,
+                15 => 1,
+                20 => 2,
+                21 => 1,
+                22 => 2,
+                23 => 1,
+                24 => 2,
+                31 => 1,
+                32 => 2,
+                33 => 2,
+                41 => 1,
+                42 => 1,
+                43 => 1,
+                44 => 1,
+                45 => 1,
+                46 => 1,
+                47 => 1,
+                48 => 1,
+                54 => 1,
+                55 => 1,
+                56 => 1,
+                57 => 1,
+                58 => 1,
+                59 => 1,
+                79 => 1,
+                80 => 1,
+                81 => 1,
+                82 => 1,
+                83 => 1,
+                85 => 1,
+                86 => 1,
+                87 => 1,
+                89 => 1,
+                90 => 1,
+                92 => 1,
+                93 => 1,
+            ];
+        case 'DisallowTabIndentUnitTest.2.inc':
+            return [
+                6  => 1,
+                7  => 1,
+                8  => 1,
+                9  => 1,
+                10 => 1,
+                11 => 1,
+                12 => 1,
+                13 => 1,
+                19 => 1,
+            ];
+        default:
+            return [];
+        }//end switch
 
     }//end getErrorList()
 
