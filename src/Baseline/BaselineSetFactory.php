54a4c522db5f1424abab71c52e48ad89f39981c3
Frank Dekker <fdekker@123inkt.nl>
2021-07-11 13:24:12 +0200

    Update file doc block

--- src/Baseline/BaselineSetFactory.php
+++ src/Baseline/BaselineSetFactory.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * An exception thrown by PHP_CodeSniffer when it wants to exit from somewhere not in the main runner.
- * Allows the runner to return an exit code instead of putting exit codes elsewhere
- * in the source code.
+ * A factory to create a baseline collection from a given file
  *
- * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Frank Dekker <fdekker@123inkt.nl>
  * @copyright 2006-2015 Squiz Pty Ltd (ABN 77 084 670 600)
  * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
  */
--- tests/Core/Baseline/BaselineSetTest.php
+++ tests/Core/Baseline/BaselineSetTest.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Tests for the \PHP_CodeSniffer\Util\Common::isCamelCaps method.
+ * Tests for the BaselineSet
  *
- * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Frank Dekker <fdekker@123inkt.nl>
  * @copyright 2006-2015 Squiz Pty Ltd (ABN 77 084 670 600)
  * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
  */
