ab84c24b89b63ad42cceff2a3ad9f98fe70f8814
jrfnl <jrfnl@users.noreply.github.com>
2021-10-06 10:51:40 +0200

    Tokenizer/PHP: fix ? tokenization after attribute
    In a select set of circumstance, when a `?` would follow an attribute with a nested array in it, the `?` would be tokenized as `T_INLINE_THEN`, not `T_NULLABLE`.
    Fixed now.
    
    Includes unit test via the `PSR12.Operators.OperatorSpacing` sniff.
    
    Note: Ternary `?` vs nullable (and ternary `:` vs colon) should really get a full set of unit tests, but I don't currently have the time to set that up.
    
    Fixes 3445

--- src/Tokenizers/PHP.php
+++ src/Tokenizers/PHP.php
@@ -1421,7 +1421,7 @@ class PHP extends Tokenizer
                         && isset(Tokens::$emptyTokens[$tokenType]) === false
                     ) {
                         // Found the previous non-empty token.
-                        if ($tokenType === ':' || $tokenType === ',') {
+                        if ($tokenType === ':' || $tokenType === ',' || $tokenType === T_ATTRIBUTE_END) {
                             $newToken['code'] = T_NULLABLE;
                             $newToken['type'] = 'T_NULLABLE';
 
