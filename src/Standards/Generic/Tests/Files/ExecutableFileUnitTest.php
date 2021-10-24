6cedf3cc9c31c27c6a02081b81aca50ac2201c70
Pascal Heidmann <pascal.heidmann@check24.de>
2021-10-24 23:35:00 +0200

    Revert unrelated ExecutableFileUnitTest.php change

--- src/Standards/Generic/Tests/Files/ExecutableFileUnitTest.php
+++ src/Standards/Generic/Tests/Files/ExecutableFileUnitTest.php
@@ -45,7 +45,7 @@ class ExecutableFileUnitTest extends AbstractSniffUnitTest
         switch ($testFile) {
         case 'ExecutableFileUnitTest.2.inc':
         case 'ExecutableFileUnitTest.4.inc':
-            return [1 => 0];
+            return [1 => 1];
         default:
             return [];
         }//end switch
