e908da764dbfa6eb6e31d908a2fa16b9c16c7944
Jaroslav Hanslík <kukulich@kukulich.cz>
2021-11-20 17:38:22 +0100

    PHP 8.1: File::getMemberProperties() detects if property is readonly

--- src/Files/File.php
+++ src/Files/File.php
@@ -1811,6 +1811,7 @@ class File
             T_PROTECTED => T_PROTECTED,
             T_STATIC    => T_STATIC,
             T_VAR       => T_VAR,
+            T_READONLY  => T_READONLY,
         ];
 
         $valid += Util\Tokens::$emptyTokens;
@@ -1818,6 +1819,7 @@ class File
         $scope          = 'public';
         $scopeSpecified = false;
         $isStatic       = false;
+        $isReadonly     = false;
 
         $startOfStatement = $this->findPrevious(
             [
@@ -1849,6 +1851,9 @@ class File
                 break;
             case T_STATIC:
                 $isStatic = true;
+                break;
+            case T_READONLY:
+                $isReadonly = true;
                 break;
             }
         }//end for
@@ -1901,6 +1906,7 @@ class File
             'scope'           => $scope,
             'scope_specified' => $scopeSpecified,
             'is_static'       => $isStatic,
+            'is_readonly'     => $isReadonly,
             'type'            => $type,
             'type_token'      => $typeToken,
             'type_end_token'  => $typeEndToken,
