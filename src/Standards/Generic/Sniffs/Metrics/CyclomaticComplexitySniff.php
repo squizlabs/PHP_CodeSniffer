cd42b1f9cbc29d3bf40f93d4c6fade302e59d1dc
Greg Sherwood <gsherwood@squiz.net>
2021-12-07 08:27:28 +1100

    Removed commented out code (ref #3495)

--- src/Standards/Generic/Sniffs/Metrics/CyclomaticComplexitySniff.php
+++ src/Standards/Generic/Sniffs/Metrics/CyclomaticComplexitySniff.php
@@ -78,8 +78,6 @@ class CyclomaticComplexitySniff implements Sniff
             T_FOR            => true,
             T_FOREACH        => true,
             T_WHILE          => true,
-            // T_DO is not required for incrementing CYC, as the terminating while in a do/while loop triggers the branch.
-            // T_DO      => true.
             T_ELSEIF         => true,
             T_INLINE_THEN    => true,
             T_COALESCE       => true,
