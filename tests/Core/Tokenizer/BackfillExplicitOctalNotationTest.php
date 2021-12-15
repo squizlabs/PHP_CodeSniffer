9a78378766f6476ea0e0d8517e4e76d4de8b7812
MarkBaker <mark@lange.demon.co.uk>
2021-12-15 12:08:18 +0100

    Rename test files to identify that this is backfill functionality

--- tests/Core/Tokenizer/BackfillExplicitOctalNotationTest.php
+++ tests/Core/Tokenizer/BackfillExplicitOctalNotationTest.php
@@ -11,7 +11,7 @@ namespace PHP_CodeSniffer\Tests\Core\Tokenizer;
 
 use PHP_CodeSniffer\Tests\Core\AbstractMethodUnitTest;
 
-class ExplicitOctalNotationTest extends AbstractMethodUnitTest
+class BackfillExplicitOctalNotationTest extends AbstractMethodUnitTest
 {
 
 
