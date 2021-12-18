0735c1c5af759b5871a61e394b332fac494c0730
jrfnl <jrfnl@users.noreply.github.com>
2021-12-18 06:24:50 +0100

    Build Phar: add custom "strip whitespace and comments" function
    This implements option 3 as discussion in the thread for PR 3442.
    
    A custom `stripWhitespaceAndComments()` function has been created and added to the script used to build the PHAR files and replaces the use of the PHP native `php_strip_whitespace()` function, which does not allow for creating PHP cross-version compatible PHAR files if attributes are used anywhere in the code.
    
    The problem with the PHP native `php_strip_whitespace()` function is this:
    * When run on PHP <= 7.4, attributes would be stripped from the code as they are seen as `#` comments. This undoes the deprecation silencing for methods for which no return type can be added yet (as needed for full PHP 8.1 compatibility).
    * When run on PHP >= 8.0, attributes are _not_ stripped, but recognized correctly, however, the function strips **all** new lines, turning the file effectively into one long line of code. This is problematic when the PHAR would subsequently be run on PHP < 8.0, as any code after the first attribute would then be seen as "commented out", leading to the PHAR not running with a parse error.
    
    To solve this, the new `stripWhitespaceAndComments()` function emulates the PHP native function, with two important differences:
    * New lines are very selectively left in the regenerated content of the files. By ensuring that there is always a new line after an attribute closer, we can prevent code from being seen as commented out in PHP < 8.0.
    * As the PHPCS native PHP tokenizer is used to interpret the file content, the token stream will be the same PHP cross-version, meaning that attributes will be recognized on all supported PHP versions and the script can now be run again on any supported PHP version and the generated PHAR files will be the same.
    
    As an additional performance tweak, `xml` files will no longer be passed to the whitespace/comment stripping. This had now effect previously and as XML files would tokenize as 100% `T_INLINE_HTML`, passing these to the new function would have no effect either (other than slowing down the script).
    Same goes for the `license.txt` file.

--- scripts/build-phar.php
+++ scripts/build-phar.php
@@ -14,12 +14,70 @@
  * @link      http://pear.php.net/package/PHP_CodeSniffer
  */
 
+use PHP_CodeSniffer\Config;
+use PHP_CodeSniffer\Exceptions\RuntimeException;
+use PHP_CodeSniffer\Exceptions\TokenizerException;
+use PHP_CodeSniffer\Tokenizers\PHP;
+use PHP_CodeSniffer\Util\Tokens;
+
 error_reporting(E_ALL | E_STRICT);
 
 if (ini_get('phar.readonly') === '1') {
     echo 'Unable to build, phar.readonly in php.ini is set to read only.'.PHP_EOL;
     exit(1);
 }
+
+require_once dirname(__DIR__).'/autoload.php';
+require_once dirname(__DIR__).'/src/Util/Tokens.php';
+
+if (defined('PHP_CODESNIFFER_VERBOSITY') === false) {
+    define('PHP_CODESNIFFER_VERBOSITY', 0);
+}
+
+
+/**
+ * Replacement for the PHP native php_strip_whitespace() function,
+ * which doesn't handle attributes correctly for cross-version PHP.
+ *
+ * @param string                  $fullpath Path to file.
+ * @param \PHP_CodeSniffer\Config $config   Perfunctory Config.
+ *
+ * @return string
+ *
+ * @throws \PHP_CodeSniffer\Exceptions\RuntimeException When tokenizer errors are encountered.
+ */
+function stripWhitespaceAndComments($fullpath, $config)
+{
+    $contents = file_get_contents($fullpath);
+
+    try {
+        $tokenizer = new PHP($contents, $config, "\n");
+        $tokens    = $tokenizer->getTokens();
+    } catch (TokenizerException $e) {
+        throw new RuntimeException('Failed to tokenize file '.$fullpath);
+    }
+
+    $stripped = '';
+    foreach ($tokens as $token) {
+        if ($token['code'] === T_ATTRIBUTE_END || $token['code'] === T_OPEN_TAG) {
+            $stripped .= $token['content']."\n";
+            continue;
+        }
+
+        if (isset(Tokens::$emptyTokens[$token['code']]) === false) {
+            $stripped .= $token['content'];
+            continue;
+        }
+
+        if ($token['code'] === T_WHITESPACE) {
+            $stripped .= ' ';
+        }
+    }
+
+    return $stripped;
+
+}//end stripWhitespaceAndComments()
+
 
 $startTime = microtime(true);
 
@@ -53,7 +111,9 @@ foreach ($scripts as $script) {
     $rdi = new \RecursiveDirectoryIterator($srcDir, \RecursiveDirectoryIterator::FOLLOW_SYMLINKS);
     $di  = new \RecursiveIteratorIterator($rdi, 0, \RecursiveIteratorIterator::CATCH_GET_CHILD);
 
+    $config    = new Config();
     $fileCount = 0;
+
     foreach ($di as $file) {
         $filename = $file->getFilename();
 
@@ -68,13 +128,19 @@ foreach ($scripts as $script) {
         }
 
         $path = 'src'.substr($fullpath, $srcDirLen);
-        $phar->addFile($fullpath, $path);
+
+        if (substr($filename, -4) === '.xml') {
+            $phar->addFile($fullpath, $path);
+        } else {
+            // PHP file.
+            $phar->addFromString($path, stripWhitespaceAndComments($fullpath, $config));
+        }
 
         ++$fileCount;
     }//end foreach
 
     // Add autoloader.
-    $phar->addFile(realpath(__DIR__.'/../autoload.php'), 'autoload.php');
+    $phar->addFromString('autoload.php', stripWhitespaceAndComments(realpath(__DIR__.'/../autoload.php'), $config));
 
     // Add licence file.
     $phar->addFile(realpath(__DIR__.'/../licence.txt'), 'licence.txt');
