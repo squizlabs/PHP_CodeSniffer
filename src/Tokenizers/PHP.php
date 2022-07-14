382ad27fbd8c8e7c1b5704d4feeb34b9c98660ce
jrfnl <jrfnl@users.noreply.github.com>
2022-07-14 15:42:34 +0200

    Tokenizer/PHP: bug fix for short list tokens in control structures without braces
    Originally reported in sirbrillig/phpcs-variable-analysis 263.
    
    When the short list syntax would be used as the first contents within a control structure without braces, the square brackets would be tokenized as plain square brackets, not as short array brackets.
    
    ```php
    if ( true )
        [ $a ] = [ 'hi' ]; // The first `[` in this line was tokenized incorrectly.
    
    return $a ?? '';
    ```
    
    Fixed now by checking whether the preceding parenthesis closer has an owner and if that owner could be a scope owner.
    
    Includes unit tests.
    Includes updating the data providers to use named data sets to allow for easier debugging.

--- src/Tokenizers/PHP.php
+++ src/Tokenizers/PHP.php
@@ -2688,13 +2688,18 @@ class PHP extends Tokenizer
                     }
 
                     if (isset(Util\Tokens::$emptyTokens[$this->tokens[$x]['code']]) === false) {
-                        if (isset($allowed[$this->tokens[$x]['code']]) === false) {
+                        // Allow for control structures without braces.
+                        if (($this->tokens[$x]['code'] === T_CLOSE_PARENTHESIS
+                            && isset($this->tokens[$x]['parenthesis_owner']) === true
+                            && isset(Util\Tokens::$scopeOpeners[$this->tokens[$this->tokens[$x]['parenthesis_owner']]['code']]) === true)
+                            || isset($allowed[$this->tokens[$x]['code']]) === false
+                        ) {
                             $isShortArray = true;
                         }
 
                         break;
                     }
-                }
+                }//end for
 
                 if ($isShortArray === true) {
                     $this->tokens[$i]['code'] = T_OPEN_SHORT_ARRAY;
