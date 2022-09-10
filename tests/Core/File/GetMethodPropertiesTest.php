a7cf635b9cd506efd47576818e45b67e0d6792fc
jrfnl <jrfnl@users.noreply.github.com>
2022-09-10 11:17:49 +0200

    GetMethodPropertiesTest: remove stray docblock opener

--- tests/Core/File/GetMethodPropertiesTest.php
+++ tests/Core/File/GetMethodPropertiesTest.php
@@ -775,7 +775,6 @@ class GetMethodPropertiesTest extends AbstractMethodUnitTest
 
 
     /**
-    /**
      * Verify recognition of PHP8.1 intersection type declaration.
      *
      * @return void
