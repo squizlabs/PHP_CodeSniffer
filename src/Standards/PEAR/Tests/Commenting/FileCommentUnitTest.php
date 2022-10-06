1a593f1243606c99bcc8b89d39633d4c0d29cdcd
jrfnl <jrfnl@users.noreply.github.com>
2022-10-07 01:14:46 +0200

    PHP 8.2 | PEAR/FileComment: allow for readonly classes
    Includes unit test.
    
    Ref:
    * https://wiki.php.net/rfc/readonly_classes

--- src/Standards/PEAR/Tests/Commenting/FileCommentUnitTest.php
+++ src/Standards/PEAR/Tests/Commenting/FileCommentUnitTest.php
@@ -48,9 +48,8 @@ class FileCommentUnitTest extends AbstractSniffUnitTest
             ];
 
         case 'FileCommentUnitTest.2.inc':
-            return [1 => 1];
-
         case 'FileCommentUnitTest.3.inc':
+        case 'FileCommentUnitTest.4.inc':
             return [1 => 1];
 
         default:
