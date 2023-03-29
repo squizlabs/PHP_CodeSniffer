8acfadab7f5a8419e287cfe73150707389b8b1a4
Dan Wallis <dan@wallis.nz>
2023-03-29 17:17:17 +0100

    Fix bug

--- src/Standards/Squiz/Sniffs/Commenting/FunctionCommentSniff.php
+++ src/Standards/Squiz/Sniffs/Commenting/FunctionCommentSniff.php
@@ -406,7 +406,7 @@ class FunctionCommentSniff extends PEARFunctionCommentSniff
 
             foreach ($typeNames as $typeName) {
                 // Strip nullable operator.
-                if ($typeName[0] === '?') {
+                if (strlen($typeName) > 1 && $typeName[0] === '?') {
                     $typeName = substr($typeName, 1);
                 }
 
